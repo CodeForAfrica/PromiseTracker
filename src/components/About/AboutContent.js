8397d2c327813478d9963f3c464fc6f38bd00fa5
nerdlet <brndnyokabi@gmail.com>
2019-09-19 18:15:11 +0300

    Fix spacing

--- src/components/About/AboutContent.js
+++ src/components/About/AboutContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Grid, Typography } from '@material-ui/core';
 
 function AboutContent() {
--- src/components/About/SideBar.js
+++ src/components/About/SideBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Grid, Typography, makeStyles } from '@material-ui/core';
 import StatusChip from '../StatusChip';
 
