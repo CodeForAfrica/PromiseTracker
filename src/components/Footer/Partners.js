08e4f8e9f3fa3bc6b0729695aabbad208329eca1
nerdlet <brndnyokabi@gmail.com>
2019-10-07 12:38:47 +0300

    Update footer section

---
+++ src/components/Footer/Partners.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { makeStyles, Grid } from '@material-ui/core';
+
+import A from '../A';
+
+import pesacheck from '../assets/images/logos/pesacheckwhite.png';
+import hivos from '../assets/images/logos/hivos.jpeg';
+import cfa from '../assets/images/logos/codeforafrica.png';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: '100%',
+    width: '100%', // 156px / 16
+    [theme.breakpoints.up('sm')]: {
+      width: '7rem'
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '7.5rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '12.5625rem' // 201px / 16
+    }
+  },
+  img: {
+    height: 'auto',
+    maxWidth: '100%'
+  }
+}));
+
+function Partners(props) {
+  const classes = useStyles(props);
+
+  return (
+    <Grid
+      container
+      className={classes.root}
+      direction="row"
+      justify="flex-start"
+    >
+      <Grid item xs={12}>
+        <A href="https://pesacheck.org/">
+          <img src={pesacheck} alt="Pesa check" className={classes.img} />
+        </A>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <A href="https://codeforkenya.org">
+            <img src={cfa} alt="Code for Africa" className={classes.img} />
+          </A>
+        </Grid>
+        <Grid item xs={6}>
+          <A href="https://www.hivos.org">
+            <img src={hivos} alt="Hivos" className={classes.img} />
+          </A>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Partners;
