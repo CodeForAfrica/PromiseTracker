73eea3c251238ab09fc7abfbf3c533a57ae28e62
nerdlet <brndnyokabi@gmail.com>
2019-11-15 14:05:53 +0300

    Fix _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,42 +1,35 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
 
-import React from "react";
-import App from "next/app";
-import Head from "next/head";
-
-import '../assets/css/App.css';
-import Theme from '../Theme'
+import 'assets/css/App.css';
 
 import { MuiThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import Theme from 'Theme';
 
 export default class PromiseTrackerApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
     }
-
-    return { pageProps };
-  }
-
-  renderHead() {
-    return (
-      <Head>
-      <title>Home - Promise Tracker</title>
-      </Head>
-    );
   }
 
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <MuiThemeProvider theme={Theme}>
-          {this.renderHead()}
+      <>
+        <Head>
+          <title>Home - Promise Tracker</title>
+        </Head>
+        <MuiThemeProvider theme={Theme}>
           <CssBaseline />
           <Component {...pageProps} />
-      </MuiThemeProvider>
+        </MuiThemeProvider>
+      </>
     );
   }
 }
