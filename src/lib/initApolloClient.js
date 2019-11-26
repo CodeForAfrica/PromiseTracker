bf46c909b7a214d19881fe5d6b81743ef7d15d50
nerdlet <brndnyokabi@gmail.com>
2019-11-26 22:29:34 +0300

    Minor tweek

--- src/lib/initApolloClient.js
+++ src/lib/initApolloClient.js
@@ -15,8 +15,8 @@ function createApolloClient(initialState = {}) {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined', // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
-      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+      uri: '', // Server URL (must be absolute)
+      credentials: '', // Additional fetch() options like `credentials` or `headers`
       fetch
     }),
     cache: new InMemoryCache().restore(initialState)
