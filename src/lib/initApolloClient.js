6d58e38fad18941edbd845cc207560d746eca323
nerdlet <brndnyokabi@gmail.com>
2019-11-26 22:28:42 +0300

    Add withApollo + initApollo

---
+++ src/lib/initApolloClient.js
@@ -0,0 +1,44 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+import fetch from 'isomorphic-unfetch';
+
+let apolloClient = null;
+
+/**
+ * Creates and configures the ApolloClient
+ * @param  {Object} [initialState={}]
+ */
+function createApolloClient(initialState = {}) {
+  // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined', // Disables forceFetch on the server (so queries are only run once)
+    link: new HttpLink({
+      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
+      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+      fetch
+    }),
+    cache: new InMemoryCache().restore(initialState)
+  });
+}
+
+/**
+ * Always creates a new apollo client on the server
+ * Creates or reuses apollo client in the browser.
+ * @param  {Object} initialState
+ */
+export default function initApolloClient(initialState) {
+  // Make sure to create a new client for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  if (typeof window === 'undefined') {
+    return createApolloClient(initialState);
+  }
+
+  // Reuse client on the client-side
+  if (!apolloClient) {
+    apolloClient = createApolloClient(initialState);
+  }
+
+  return apolloClient;
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from 'next/app';
-import Head from 'next/head';
 
 import 'assets/css/App.css';
 
@@ -8,6 +7,9 @@ import { MuiThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import theme from 'theme';
+
+import withApollo from 'lib/withApollo';
+import { ApolloProvider } from 'react-apollo';
 
 class PromiseTrackerApp extends App {
   componentDidMount() {
@@ -19,24 +21,18 @@ class PromiseTrackerApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { PageComponent, pageProps, apolloClient } = this.props;
     return (
       <>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-          <title>Home - Promise Tracker</title>
-        </Head>
         <MuiThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
+          <ApolloProvider client={apolloClient}>
+            <CssBaseline />
+            <PageComponent {...pageProps} />
+          </ApolloProvider>
         </MuiThemeProvider>
       </>
     );
   }
 }
 
-export default PromiseTrackerApp;
+export default withApollo(PromiseTrackerApp);
