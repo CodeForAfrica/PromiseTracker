08f624f4e127e8852da2a93373956b317a1ba63a
Clemence Kyara <clemence@codeforafrica.org>
2019-12-04 15:23:13 +0300

    Lint

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
-module.exports = withCSS(withImages({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+module.exports = withCSS(
+  withImages({
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
-    return config
-  }
-
-}));
+      return config;
+    }
+  })
+);
