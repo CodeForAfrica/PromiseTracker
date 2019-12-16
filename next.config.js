90e5b39eaa8a6cefa6f5fb88f32c9eb38c730579
nerdlet <brndnyokabi@gmail.com>
2019-12-16 13:49:48 +0300

    Remove webpack config && dotenv

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
 
-module.exports = withCSS(
-  withImages({
-    webpack(config) {
-      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
-      return config;
-    }
-  })
-);
+module.exports = withCSS(withImages());
