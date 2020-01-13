890a5bad09fb3086bde7d8ea01f89ff6c793ee1a
nerdlet <brndnyokabi@gmail.com>
2020-01-13 12:07:57 +0300

    Get count of status in graphql

--- src/components/StatusPieChart.js
+++ src/components/StatusPieChart.js
@@ -4,6 +4,53 @@ import { PieChart, Pie, Cell, LabelList } from 'recharts';
 import { Grid, Typography, makeStyles } from '@material-ui/core';
 
 import chartData from 'data';
+import slugify from 'lib/slugify';
+
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_CHART_DATA = gql`
+  query {
+    team {
+      id
+      name
+      projects {
+        edges {
+          node {
+            id
+            title
+            project_medias(last: 6) {
+              edges {
+                node {
+                  id
+                  dbid
+                  title
+                  tasks {
+                    edges {
+                      node {
+                        id
+                        label
+                        first_response_value
+                      }
+                    }
+                  }
+                  tags {
+                    edges {
+                      node {
+                        id
+                        tag_text
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const totalPromises = chartData.chartPromises.reduce((a, b) => a + b.value, 0);
 
@@ -31,13 +78,51 @@ const useStyles = makeStyles(theme => ({
 
 function StatusPieChart() {
   const classes = useStyles();
+
+  const { loading, error, data } = useQuery(GET_CHART_DATA);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return null;
+  }
+
+  // Get  total medias
+  const promises = data.team.projects.edges.map(({ node: project }) => project);
+  const medias = promises[0].project_medias.edges.map(
+    ({ node: media }) => media
+  );
+
+  const mediaArray = medias.map(media => ({
+    title: media.title,
+    status: slugify(
+      media.tasks.edges.find(
+        ({ node: task }) => task.label === 'What is the status of the promise?'
+      ).node.first_response_value
+    )
+  }));
+
+  // const newCount = mediaArray.reduce(
+  // (c, { status: key }) => ((c[key] = (c[key] || 0) + 1), c),
+  // {}
+  // );
+
+  /* eslint-disable no-param-reassign */
+  const newCount = mediaArray.reduce((c, { status: key }) => {
+    c[key] = (c[key] || 0) + 1;
+    return c;
+  }, {});
+
+  console.log(newCount);
+
   const [activeData, setActiveData] = useState(null);
-  const onMouseEnter = data => {
-    setActiveData(data);
+  const onMouseEnter = newData => {
+    setActiveData(newData);
   };
   const onMouseLeave = () => {
     setActiveData(null);
   };
+
   return (
     <div className={classes.root}>
       <Grid
