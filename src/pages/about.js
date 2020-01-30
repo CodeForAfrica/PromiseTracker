fcb4bda953726920270cd7ae6b0f613aa5c72d55
nerdlet <brndnyokabi@gmail.com>
2020-01-30 10:46:30 +0300

    Remove implementation

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,70 +8,14 @@ import AboutContent from 'components/About/Content';
 import SideBar from 'components/About/SideBar';
 import Layout from 'components/Layout';
 
-import withApollo from 'lib/withApollo';
-import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
-
 const useStyles = makeStyles({
   root: {
     padding: '8rem 0'
   }
 });
 
-const GET_PROMISES = gql`
-  query {
-    team(slug: "pesacheck-promise-tracker") {
-      id
-      name
-      projects {
-        edges {
-          node {
-            id
-            title
-            project_medias(last: 6) {
-              edges {
-                node {
-                  id
-                  dbid
-                  title
-                  tasks {
-                    edges {
-                      node {
-                        id
-                        label
-                        first_response_value
-                      }
-                    }
-                  }
-                  tags {
-                    edges {
-                      node {
-                        id
-                        tag_text
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
 function About() {
   const classes = useStyles();
-
-  const { loading, error, data } = useQuery(GET_PROMISES);
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return null;
-  }
-  console.log(data);
 
   return (
     <>
@@ -96,4 +40,4 @@ function About() {
   );
 }
 
-export default withApollo(About);
+export default About;
