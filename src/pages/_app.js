b49c3829dc79b4ce91dabc6ddeb7171fdc814f20
nerdlet <brndnyokabi@gmail.com>
2020-01-31 16:38:59 +0300

    Add metatags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,32 @@ import theme from 'theme';
 import { MuiThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import NextSeo from 'next-seo';
+
+const SEO_METATAGS = {
+  title: 'Promise Tracker App',
+  description: '',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: '',
+    title: 'Next.js Seo',
+    description: '',
+    image: '',
+    site_name: 'promise-tracker',
+    imageWidth: 1200,
+    imageHeight: 1200
+  },
+  twitter: {
+    handle: '',
+    cardType: ''
+  },
+  facebook: {
+    handle: '',
+    cardType: ''
+  }
+};
+
 class PromiseTrackerApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
@@ -22,6 +48,7 @@ class PromiseTrackerApp extends App {
       <>
         <MuiThemeProvider theme={theme}>
           <CssBaseline />
+          <NextSeo config={SEO_METATAGS} />
           <Component {...pageProps} />
         </MuiThemeProvider>
       </>
