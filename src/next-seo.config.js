809924156b488f342aad25227d16ef39129a944c
nerdlet <brndnyokabi@gmail.com>
2020-02-03 13:58:14 +0300

    Add to config

---
+++ src/next-seo.config.js
@@ -0,0 +1,24 @@
+import openGraph from 'assets/images/openGraph.png';
+
+export default {
+  title: 'Promise Tracker',
+  description:
+    'PromiseTracker is a platform-based promise tracker where Kenyan citizens can track various promises made by governors in their manifestos and during the campaigns leading up to the 2017 general election',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://promisetracker.codeforafrica.org/',
+    title: 'Promise tracker',
+    description:
+      'PromiseTracker, is a tool to help journalists and civil society watchdogs more easily track campaign promises and other political / government pledges',
+    image: { openGraph },
+    site_name: 'Promise Tracker',
+    imageWidth: 1200,
+    imageHeight: 1200
+  },
+  twitter: {
+    handle: '@PesaCheck',
+    site: '@PesaCheck',
+    cardType: 'summary_large_image'
+  }
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,30 +8,7 @@ import { MuiThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import NextSeo from 'next-seo';
-import openGraph from 'assets/images/openGraph.png';
-
-const SEO_METATAGS = {
-  title: 'Promise Tracker App',
-  description:
-    'PromiseTracker is a platform-based promise tracker where Kenyan citizens can track various promises made by governors in their manifestos and during the campaigns leading up to the 2017 general election',
-  openGraph: {
-    type: 'website',
-    locale: 'en_IE',
-    url: 'https://promisetracker.codeforafrica.org/',
-    title: 'Promise tracker',
-    description:
-      'PromiseTracker, is a tool to help journalists and civil society watchdogs more easily track campaign promises and other political / government pledges',
-    image: { openGraph },
-    site_name: 'Promise Tracker',
-    imageWidth: 1200,
-    imageHeight: 1200
-  },
-  twitter: {
-    handle: '@Code4Africa',
-    site: '@Code4Africa',
-    cardType: 'summary_large_image'
-  }
-};
+import SEO from '../next-seo.config';
 
 class PromiseTrackerApp extends App {
   componentDidMount() {
@@ -48,7 +25,7 @@ class PromiseTrackerApp extends App {
       <>
         <MuiThemeProvider theme={theme}>
           <CssBaseline />
-          <NextSeo config={SEO_METATAGS} />
+          <NextSeo config={SEO} />
           <Component {...pageProps} />
         </MuiThemeProvider>
       </>
