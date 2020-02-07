c16b45d24c4145d5d3c7f0868dc790cf4db0598a
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-02-07 11:36:06 +0300

    Minor headers change

--- src/lib/withApollo.js
+++ src/lib/withApollo.js
@@ -16,16 +16,17 @@ let reusableApolloClient = null;
 function create(initialState) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
   const isBrowser = typeof window !== 'undefined';
+  const headers = isBrowser && {
+    'X-Check-Token': process.env.CHECK_ACCESS_TOKEN,
+    Origin: config.url,
+    'X-Requested-With': 'XMLHttpRequest'
+  };
   return new ApolloClient({
     connectToDevTools: isBrowser,
     ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
+      headers,
       uri: config.PROXY_URL + config.GRAPHQL_URI, // Server URL (must be absolute)
-      headers: {
-        'X-Check-Token': process.env.CHECK_ACCESS_TOKEN,
-        Origin: config.url,
-        'X-Requested-With': 'XMLHttpRequest'
-      },
       // Use fetch() polyfill on the server
       fetch: !isBrowser && fetch
     }),
