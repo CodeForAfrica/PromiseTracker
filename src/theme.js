aea6da6cbb9d2a5d94aa62611ba13f129c62b3bc
Clemence Kyara <clemence@codeforafrica.org>
2020-02-12 11:52:10 +0300

    Add minimal theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,32 @@
-import { createMuiTheme } from '@material-ui/core';
+import createTheme from '@codeforafrica/hurumap-ui/core/styles/createTheme';
 
-const theme = createMuiTheme({});
+const FONT_FAMILY_TEXT = '"Roboto", "sans-serif"';
+
+const COLOR_SCALE = [];
+
+const theme = createTheme({
+  chart: {
+    colorScale: COLOR_SCALE,
+    pie: {
+      colorScale: COLOR_SCALE,
+      height: 350,
+      legendWidth: 150,
+      origin: { x: 150, y: 125 },
+      padding: 0,
+      style: {
+        data: {
+          fontFamily: FONT_FAMILY_TEXT,
+          fontSize: 10
+        },
+        labels: {
+          fontFamily: FONT_FAMILY_TEXT,
+          fontSize: 16,
+          fill: 'rgb(0,0,0)'
+        }
+      },
+      width: 350
+    }
+  }
+});
 
 export default theme;
