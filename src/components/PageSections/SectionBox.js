cafd4cc3f8ddb15759a8e1b250f1b56584479ff4
nerdlet <brndnyokabi@gmail.com>
2020-09-02 19:01:03 +0300

    Delete files

--- src/components/PageSections/SectionBox.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-
-function SectionBox(props) {
-  return <Box component="div" pt={9} pb={9} {...props} />;
-}
-
-export default SectionBox;
--- src/lib/findStatus.js
+++
@@ -1,8 +0,0 @@
-import config from 'config';
-
-export default function findStatus(statusParam) {
-  const foundEdge = statusParam.tasks.edges.find(
-    ({ node: task }) => task.label === config.status.label
-  );
-  return (foundEdge && foundEdge.node.first_response_value) || '';
-}
