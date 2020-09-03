2ac639cf98f97e6b2dd5c721ecfc00b3931cff84
nerdlet <brndnyokabi@gmail.com>
2020-09-03 10:50:00 +0300

    Update .babelrc

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import { Typography } from '@material-ui/core';
 import MuiLink from '@material-ui/core/Link';
 
 export default function Copyright() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { Container, Typography, Box } from '@material-ui/core';
 import Copyright from '@/promiseTracker/components/Copyright';
 
 export default function Index() {
