0e8dc64d2f7098d6a064dc904ae306d44baf9b2e
nerdlet <brndnyokabi@gmail.com>
2020-09-03 10:00:38 +0300

    Change imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Copyright from '../../src/Copyright';
+import Copyright from '@/promiseTracker/Copyright';
 
 export default function Index() {
   return (
