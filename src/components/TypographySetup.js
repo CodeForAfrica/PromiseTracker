fa32062d0543b728c78922e140cede44d27f94f5
nerdlet <brndnyokabi@gmail.com>
2020-09-08 13:12:08 +0300

    Add font size

--- src/components/TypographySetup.js
+++ src/components/TypographySetup.js
@@ -5,27 +5,33 @@ export default function TypographySetup() {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
       <Typography variant="h1" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H1: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
 
       <Typography variant="h2" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H2: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
 
       <Typography variant="h3" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H3: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
 
       <Typography variant="h4" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H4: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
 
       <Typography variant="h5" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H5: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
 
       <Typography variant="h6" align="center" style={{ padding: "1rem 0rem" }}>
-        Lorem ipsum dolor sit amet, consectetur
+        <em style={{ color: "#005DFD" }}>H6: </em> Lorem ipsum dolor sit amet,
+        consectetur
       </Typography>
       <hr />
 
@@ -34,9 +40,10 @@ export default function TypographySetup() {
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>body1: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
 
       <Typography
@@ -44,9 +51,10 @@ export default function TypographySetup() {
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>body2: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
 
       <Typography
@@ -54,9 +62,10 @@ export default function TypographySetup() {
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>subtitle1: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
 
       <Typography
@@ -64,9 +73,10 @@ export default function TypographySetup() {
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>subtitle2: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
 
       <Typography
@@ -74,19 +84,22 @@ export default function TypographySetup() {
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>caption: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
 
       <Typography
         variant="button"
         align="center"
+        color="textSecondary"
         style={{ padding: "1rem 0rem" }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        venenatis tincidunt quam. Etiam venenatis, nulla a hendrerit
-        ullamcorper,massa libero lobortis nibh, onsequat augue velit ac lectus.
+        <em style={{ color: "#005DFD" }}>button: </em> Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Curabitur venenatis tincidunt quam.
+        Etiam venenatis, nulla a hendrerit ullamcorper,massa libero lobortis
+        nibh, onsequat augue velit ac lectus.
       </Typography>
     </Grid>
   );
