98dc7eee2ec535bfd39a6496dd28f81ff6511fe9
nerdlet <brndnyokabi@gmail.com>
2020-09-08 13:02:50 +0300

    Remove typography color

--- src/components/TypographySetup.js
+++ src/components/TypographySetup.js
@@ -4,64 +4,33 @@ import { Grid, Typography } from "@material-ui/core";
 export default function TypographySetup() {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
-      <Typography
-        variant="h1"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h1" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
 
-      <Typography
-        variant="h2"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h2" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
 
-      <Typography
-        variant="h3"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h3" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
 
-      <Typography
-        variant="h4"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h4" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
 
-      <Typography
-        variant="h5"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h5" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
 
-      <Typography
-        variant="h6"
-        color="textSecondary"
-        align="center"
-        style={{ padding: "1rem 0rem" }}
-      >
+      <Typography variant="h6" align="center" style={{ padding: "1rem 0rem" }}>
         Lorem ipsum dolor sit amet, consectetur
       </Typography>
       <hr />
 
       <Typography
         variant="body1"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
@@ -72,7 +41,6 @@ export default function TypographySetup() {
 
       <Typography
         variant="body2"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
@@ -83,7 +51,6 @@ export default function TypographySetup() {
 
       <Typography
         variant="subtitle1"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
@@ -94,7 +61,6 @@ export default function TypographySetup() {
 
       <Typography
         variant="subtitle2"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
@@ -105,7 +71,6 @@ export default function TypographySetup() {
 
       <Typography
         variant="caption"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
@@ -116,7 +81,6 @@ export default function TypographySetup() {
 
       <Typography
         variant="button"
-        color="textSecondary"
         align="center"
         style={{ padding: "1rem 0rem" }}
       >
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Index() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-        <Typography variant="h1" component="h1" gutterBottom>
+        <Typography variant="h4" gutterBottom>
           Next.js theme setup
         </Typography>
         <TypographySetup />
