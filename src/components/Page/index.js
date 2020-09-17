8dfb4ed5fc9378c04aabef35834639c46876b4d5
nerdlet <brndnyokabi@gmail.com>
2020-09-17 15:23:36 +0300

    Revert "Update page component"
    This reverts commit 0ee6c846d628a24ccd8ea5b271fac48ecd372e96.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -4,35 +4,16 @@ import PropTypes from "prop-types";
 import Error from "next/error";
 import { NextSeo } from "next-seo";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-import Navigation from "@/promisetracker/components/Navigation";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    // Seems like you need height defined for AppBar position="sticky" to work
-    // see: https://github.com/mui-org/material-ui/issues/16186
-    height: "100vh",
-    // font-boosting: https://stackoverflow.com/questions/13430897/how-to-override-font-boosting-in-mobile-chrome#comment61478376_16432702
-    maxHeight: 999999,
-    overflowX: "hidden",
-  },
-  section: {
-    paddingTop: 0,
-  },
-}));
 function Page({ children, errorCode, ...props }) {
-  const classes = useStyles(props);
   if (errorCode) {
     return <Error statusCode={errorCode} />;
   }
 
   return (
-    <div className={classes.root}>
+    <>
       <NextSeo {...props} />
-      <Navigation classes={{ section: classes.section }} />
       {children}
-    </div>
+    </>
   );
 }
 
