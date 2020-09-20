8dcdf2c28612eadb1d8253c732368b7e0b3dd42b
Obed Amoasi <obedamoasi@gmail.com>
2020-09-20 09:56:47 +0000

    add author component

---
+++ src/components/Article/Author.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(({ typography, palette }) => ({
+  root: {
+    alignItems: "center",
+    display: "flex",
+  },
+  dateReadTime: {
+    color: palette.secondary.dark,
+  },
+  dateReadTimeContainer: {},
+  image: {
+    width: typography.pxToRem(61),
+    height: typography.pxToRem(39),
+    marginRight: "1rem",
+    background: (props) =>
+      props.image ? `url(${props.image})` : palette.secondary.dark,
+  },
+  name: {},
+}));
+function Author({ author, date, readTime, classes: classesProp }) {
+  const classes = useStyles({ image: author.image, classes: classesProp });
+  return (
+    <div className={classes.root}>
+      <div className={classes.image} />
+      <div className={classes.dateReadTimeContainer}>
+        <Typography className={classes.name} variant="h4">
+          {author.name}
+        </Typography>
+        <Typography className={classes.dateReadTime} variant="h6">
+          {date},{readTime}
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+Author.propTypes = {
+  classes: PropTypes.shape({
+    dateReadTime: PropTypes.string,
+    dateReadTimeContainer: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    root: PropTypes.string,
+  }),
+  author: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+  }),
+  date: PropTypes.string,
+  readTime: PropTypes.string,
+};
+
+Author.defaultProps = {
+  classes: undefined,
+  author: {
+    name: "Pesa Check",
+    image: "",
+  },
+  date: "Jun 04",
+  readTime: "5 min read",
+};
+
+export default Author;
