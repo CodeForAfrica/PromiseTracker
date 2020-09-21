46b90f014f7d4d55c2de5669b9b8ff67d5431c44
nerdlet <brndnyokabi@gmail.com>
2020-09-21 14:13:22 +0300

    Add mobile svg

--- src/components/Hero/Chart/MobIleChart/index.js
+++ src/components/Hero/Chart/MobIleChart/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Grid, Typography } from "@material-ui/core";
 
 function MobileChart() {
-  return <div> example mobile chart</div>;
+  return (
+    <>
+      <Grid container item xs={12} direction="row">
+        <Grid item xs={3}>
+          <Typography variant="caption">Promise Kept</Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <svg width={50} height={50}>
+            <circle
+              cx={25}
+              cy={25}
+              r={10}
+              fill="#145BD5"
+              stroke="#1D1D1B"
+              strokeWidth="1"
+            />
+          </svg>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default MobileChart;
