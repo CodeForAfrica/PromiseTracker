eeddae28b84117edbb998bb9fa9ab4912e26c0bd
Clemence Kyara <clemence@codeforafrica.org>
2020-09-21 13:36:53 +0300

    Articles, FactChecks & Resources should be in /analysis/

--- src/pages/analysis/articles.js
+++ src/pages/analysis/articles.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { Container, Box } from "@material-ui/core";
+
+import { Box, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 import Page from "@/promisetracker/components/Page";
 
-export default function Articles() {
+const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
+  section: {
+    padding: `0 ${typography.pxToRem(23)}`,
+    margin: 0,
+    width: "100%",
+    [breakpoints.up("lg")]: {
+      padding: 0,
+      margin: "0 auto",
+      width: typography.pxToRem(widths.values.lg),
+    },
+  },
+}));
+
+function Articles(props) {
+  const classes = useStyles(props);
+
   return (
-    <Page>
+    <Page title="Articles | Analysis" classes={{ section: classes.section }}>
       <Container maxWidth="sm">
         <Box my={4}>Articles Page</Box>
       </Container>
     </Page>
   );
 }
+
+export default Articles;
