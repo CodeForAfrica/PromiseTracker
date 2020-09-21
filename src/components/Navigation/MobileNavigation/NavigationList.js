d1bfa56cbd9d3c2dc9e27ef0d39313a1e3c5e47b
Clemence Kyara <clemence@codeforafrica.org>
2020-09-21 13:38:37 +0300

    Fix NavigationList

--- src/components/Navigation/MobileNavigation/NavigationList.js
+++ src/components/Navigation/MobileNavigation/NavigationList.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 import { Grid, List, ListItem, Collapse, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Link from "@/promisetracker/components/Link";
 
-import config from "@/promisetracker/config";
-
 const useStyles = makeStyles(({ typography, palette }) => ({
   root: {},
   collapse: {
-    margin: "1rem",
     borderLeft: `1.5px solid #707070`,
+    margin: 0,
+    padding: 0,
   },
   listItem: {
+    padding: 0,
+    paddingTop: typography.pxToRem(55),
     "&:hover": {
       backgroundColor: "unset",
     },
+    "&:first-of-type": {
+      paddingTop: typography.pxToRem(40),
+    },
   },
   listItemText: {
-    paddingTop: "2rem",
+    paddingTop: typography.pxToRem(50),
     color: palette.background.default,
     fontSize: typography.pxToRem(18),
     textTransform: "uppercase",
@@ -28,28 +33,24 @@ const useStyles = makeStyles(({ typography, palette }) => ({
     fontFamily: typography.fontFamily,
     padding: "0rem 1rem",
   },
-  indexlistItemText: {
-    fontSize: typography.pxToRem(18),
-    color: palette.background.default,
-    textTransform: "uppercase",
-    fontWeight: 600,
-    letterSpacing: "0.72px",
-    fontFamily: typography.fontFamily,
-    padding: "1rem 1rem 0rem 1rem",
+  mainNav: {
+    padding: 0,
   },
   openTitle: {
     color: palette.secondary.main,
     fontSize: typography.pxToRem(13),
-    textTransform: "uppercase",
     fontWeight: 700,
     letterSpacing: "0.52px",
+    padding: 0,
+    opacity: 0.5,
+    textTransform: "uppercase",
   },
   title: {
     color: palette.background.default,
     fontSize: typography.pxToRem(18),
-    textTransform: "uppercase",
     fontWeight: 600,
-    padding: "1rem 0rem",
+    padding: 0,
+    textTransform: "uppercase",
   },
   items: {
     padding: "0rem 1.5rem",
@@ -61,15 +62,16 @@ function ListItemLink(props) {
   return <Link {...props} variant="h4" />;
 }
 
-function NavigationList(props) {
+function NavigationList({ onNavigate, open: openProp, navigation, ...props }) {
   const classes = useStyles(props);
-  const { analysisMenu } = config;
-
-  const [open, setOpen] = React.useState(true);
-
+  const [open, setOpen] = useState(openProp);
   const handleClick = () => {
     setOpen(!open);
   };
+
+  if (!navigation?.length) {
+    return null;
+  }
   return (
     <Grid
       container
@@ -77,16 +79,22 @@ function NavigationList(props) {
       justify="space-between"
       className={classes.root}
     >
-      <List component="nav" aria-labelledby="nested-list-subheader">
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        className={classes.mainNav}
+      >
         <ListItem
           component="div"
           autoFocus={false}
-          button
+          onClick={onNavigate}
           className={classes.listItem}
         >
-          <Typography variant="h4" className={classes.title}>
-            Promises
-          </Typography>
+          <ListItemLink underline="none" href="/promises">
+            <Typography variant="h4" className={classes.title}>
+              Promises
+            </Typography>
+          </ListItemLink>
         </ListItem>
 
         <ListItem
@@ -97,27 +105,22 @@ function NavigationList(props) {
         >
           <Typography
             variant="h4"
-            className={open ? classes.title : classes.openTitle}
+            className={open ? classes.openTitle : classes.title}
           >
             Analysis
           </Typography>
         </ListItem>
 
-        <Collapse in={!open} timeout="auto" unmountOnExit>
+        <Collapse in={open} timeout="auto">
           <List component="nav" className={classes.collapse}>
-            {analysisMenu.map((item, i) => (
+            {navigation.map((item) => (
               <ListItemLink
                 key={item.name}
-                underline="none"
                 href={item.href}
-                as={item.href}
+                onClick={onNavigate}
+                underline="none"
               >
-                <Typography
-                  variant="h4"
-                  className={
-                    i === 0 ? classes.indexlistItemText : classes.listItemText
-                  }
-                >
+                <Typography variant="h4" className={classes.listItemText}>
                   {item.name}
                 </Typography>
               </ListItemLink>
@@ -128,16 +131,34 @@ function NavigationList(props) {
         <ListItem
           component="div"
           autoFocus={false}
-          button
+          onClick={onNavigate}
           className={classes.listItem}
         >
-          <Typography variant="h4" className={classes.title}>
-            Act Now
-          </Typography>
+          <ListItemLink underline="none" href="/act-now">
+            <Typography variant="h4" className={classes.title}>
+              Act Now
+            </Typography>
+          </ListItemLink>
         </ListItem>
       </List>
     </Grid>
   );
 }
+
+NavigationList.propTypes = {
+  navigation: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onNavigate: PropTypes.func,
+  open: PropTypes.bool,
+};
+
+NavigationList.defaultProps = {
+  onNavigate: undefined,
+  open: undefined,
+};
 
 export default NavigationList;
