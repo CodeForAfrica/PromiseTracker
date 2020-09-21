f13bf03867ac7d764963c88bdee727317f6fd91c
Clemence Kyara <clemence@codeforafrica.org>
2020-09-21 13:40:32 +0300

    Make DesktopNavigation sticky + handle Section

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,34 +2,40 @@ import React from "react";
 
 import { AppBar, Toolbar, useMediaQuery } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import DesktopNavigation from "@/promisetracker/components/Navigation/DesktopNavigation";
-import MobileNavigation from "@/promisetracker/components/Navigation/MobileNavigation";
+
+import DesktopNavigation from "./DesktopNavigation";
+import MobileNavigation from "./MobileNavigation";
 
 const useStyles = makeStyles(({ breakpoints, palette }) => ({
   root: {
     backgroundColor: palette.background.default,
-    boxShadow: "0px 8px 30px #0000001A",
+    boxShadow: "0px 10px 40px #0000002E;",
     [breakpoints.up("md")]: {
-      boxShadow: "0px 8px 30px #0000001A",
+      boxShadow: "0px 8px 30px #0000001A;",
     },
   },
+  section: {},
   toolbar: {
-    height: "100%",
     [breakpoints.up("md")]: {
       height: "auto",
     },
   },
 }));
 
-function Navigation() {
-  const classes = useStyles();
+function Navigation(props) {
+  const classes = useStyles(props);
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
   return (
     <>
-      <AppBar color="primary" position="fixed" className={classes.root}>
+      <AppBar color="primary" position="sticky" className={classes.root}>
         <Toolbar disableGutters className={classes.toolbar}>
-          {isDesktop ? <DesktopNavigation /> : <MobileNavigation />}
+          {isDesktop ? (
+            <DesktopNavigation classes={{ section: classes.section }} />
+          ) : (
+            <MobileNavigation classes={{ section: classes.section }} />
+          )}
         </Toolbar>
       </AppBar>
     </>
