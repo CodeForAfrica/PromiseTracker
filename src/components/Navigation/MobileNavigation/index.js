9cd41c003eb38f717e1306b1b8f734ea88901a1f
Clemence Kyara <clemence@codeforafrica.org>
2020-09-21 13:38:59 +0300

    Improve MobileNavigation to handle current url

--- src/components/Navigation/MobileNavigation/index.js
+++ src/components/Navigation/MobileNavigation/index.js
@@ -1,64 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
 import clsx from "clsx";
 
-import { makeStyles } from "@material-ui/core/styles";
+import { useRouter } from "next/router";
 
 import {
-  Grid,
-  Slide,
-  IconButton,
+  Button,
   Dialog,
   DialogActions,
   DialogContent,
-  Link,
+  Grid,
+  IconButton,
+  Slide,
   Typography,
 } from "@material-ui/core";
+import { Close as CloseIcon, Menu as MenuIcon } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
-import SearchIcon from "@material-ui/icons/Search";
-import CloseIcon from "@material-ui/icons/Close";
-import MenuIcon from "@material-ui/icons/Menu";
-
-import IconButtonLink from "@/promisetracker/components/Link/IconButton";
+import { Section } from "@commons-ui/core";
 
 import Logo from "@/promisetracker/components/Navigation/Logo";
 import NavigationList from "@/promisetracker/components/Navigation/MobileNavigation/NavigationList";
+import SearchIcon from "@/promisetracker/icons/Search";
 
 import config from "@/promisetracker/config";
 
 const useStyles = makeStyles(({ palette, typography }) => ({
-  root: {},
-  dialog: {},
+  root: {
+    padding: `${typography.pxToRem(25 - 4)} 0 ${typography.pxToRem(27 - 4)} 0`,
+  },
+  section: {},
+  closeButton: {
+    marginRight: "-1rem",
+  },
+  dialog: {
+    padding: 0,
+  },
   dialogActions: {
-    padding: "8px 24px",
+    padding: `0 ${typography.pxToRem(21.5)} 0 ${typography.pxToRem(33)}`,
     transform: "matrix(-1, 0, 0, -1, 0, 0, )",
     background:
       "transparent linear-gradient(360deg, #202020 -200%, #005DFD 100%) 0% 0% no-repeat padding-box",
   },
   dialogContent: {
+    padding: `0 ${typography.pxToRem(21.5)} 0 ${typography.pxToRem(33)}`,
     backgroundColor: "#005DFD",
   },
-  menuIcon: {
+  menuButton: {
     color: palette.primary.main,
+    padding: 0,
+    paddingLeft: typography.pxToRem(12),
+    "&:hover": {
+      background: "inherit",
+    },
   },
-  searchIcon: {
-    color: "#D6D6D6",
+  searchButton: {
+    paddingBottom: 0,
+    paddingTop: 0,
+    "&:hover": {
+      background: "inherit",
+    },
   },
   button: {
     color: palette.background.default,
     padding: "1rem",
   },
   languageButton: {
-    fontWeight: "normal",
     color: palette.primary.main,
     fontFamily: typography.fontFamily,
-    textTransform: "capitalize",
     fontSize: typography.pxToRem(14),
+    fontWeight: 400,
+    minWidth: 0,
     opacity: 0.5,
+    textTransform: "capitalize",
     "&.active": {
       opacity: 1.0,
       color: palette.background.default,
     },
+  },
+  firstLanguageButton: {
+    marginLeft: "-1rem",
   },
 }));
 
@@ -68,16 +89,12 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 function MobileNavigation(props) {
   const classes = useStyles(props);
-  const [open, setOpen] = React.useState(false);
-  const { navigationMenu } = config;
-
-  const activeclassName = clsx(
-    classes.button,
-    classes.languageButton,
-    "active"
-  );
-  const className = clsx(classes.button, classes.languageButton);
-
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const { asPath } = router;
+  const { analysisMenu } = config;
+  const currentPageUrl = asPath.split("/").slice(0, 2).join("/");
+  const openAnalysisMenu = analysisMenu.url === currentPageUrl;
   const handleClickOpen = (e) => {
     if (e) {
       e.preventDefault();
@@ -90,37 +107,45 @@ function MobileNavigation(props) {
     }
     setOpen(false);
   };
+  const handleNavigate = () => {
+    setOpen(false);
+  };
 
+  const activeLanguageButton = clsx(
+    classes.button,
+    classes.languageButton,
+    "active"
+  );
+  const languageButton = clsx(classes.button, classes.languageButton);
   return (
-    <Grid
-      container
-      justify="space-between"
-      alignItems="center"
-      className={classes.root}
-    >
-      <Grid item xs={3}>
-        <Logo />
-      </Grid>
-      <Grid item>
-        <IconButtonLink
-          aria-label="Open drawer"
-          color="secondary"
-          edge="start"
-          href="/#"
-          className={classes.searchIcon}
-        >
-          <SearchIcon fontSize="large" />
-        </IconButtonLink>
-
-        <IconButtonLink
-          aria-label="Open drawer"
-          edge="start"
-          href="/#"
-          onClick={handleClickOpen}
-          className={classes.menuIcon}
-        >
-          <MenuIcon fontSize="large" />
-        </IconButtonLink>
+    <Section classes={{ root: classes.section }}>
+      <Grid
+        container
+        justify="space-between"
+        alignItems="flex-end"
+        className={classes.root}
+      >
+        <Grid item>
+          <Logo />
+        </Grid>
+        <Grid item>
+          <IconButton
+            aria-label="Search"
+            color="secondary"
+            edge="start"
+            className={classes.searchButton}
+          >
+            <SearchIcon viewBox="0 0 24 16" htmlColor="#D6D6D6" />
+          </IconButton>
+          <IconButton
+            aria-label="Open drawer"
+            edge="start"
+            onClick={handleClickOpen}
+            className={classes.menuButton}
+          >
+            <MenuIcon viewBox="0 0 24 16" />
+          </IconButton>
+        </Grid>
 
         <Dialog
           fullScreen
@@ -130,67 +155,80 @@ function MobileNavigation(props) {
           classes={{ root: classes.dialog, paper: classes.dialogPaper }}
         >
           <DialogActions className={classes.dialogActions}>
-            <Grid
-              container
-              justify="space-between"
-              alignItems="center"
-              spacing={1}
-            >
-              <Grid item>
-                <Link
-                  href="/#"
-                  variant="overline"
-                  underline="none"
-                  className={activeclassName}
-                >
-                  En
-                </Link>
-                <Link
-                  href="/#"
-                  variant="overline"
-                  underline="none"
-                  className={className}
-                >
-                  Am
-                </Link>
-                <Link
-                  href="/#"
-                  variant="overline"
-                  underline="none"
-                  className={className}
-                >
-                  Fr
-                </Link>
-                <Link
-                  href="/#"
-                  variant="overline"
-                  underline="none"
-                  className={className}
-                >
-                  عربى
-                </Link>
-              </Grid>
-            </Grid>
-
-            <Grid item>
-              <IconButton
-                aria-label="close drawer"
-                edge="start"
-                onClick={handleClose}
-                className={classes.button}
+            <Grid container justify="space-between">
+              <Grid
+                item
+                xs={10}
+                container
+                justify="flex-start"
+                alignItems="center"
               >
-                <CloseIcon fontSize="large" />
-              </IconButton>
+                <Grid item>
+                  <Button
+                    href="/#"
+                    variant="overline"
+                    underline="none"
+                    className={clsx(
+                      activeLanguageButton,
+                      classes.firstLanguageButton
+                    )}
+                  >
+                    En
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="overline"
+                    underline="none"
+                    className={languageButton}
+                  >
+                    Am
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="overline"
+                    underline="none"
+                    className={languageButton}
+                  >
+                    Fr
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="overline"
+                    underline="none"
+                    className={languageButton}
+                  >
+                    عربى
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid item xs={2} container justify="flex-end">
+                <IconButton
+                  aria-label="close drawer"
+                  edge="start"
+                  onClick={handleClose}
+                  className={clsx(classes.button, classes.closeButton)}
+                >
+                  <CloseIcon fontSize="large" />
+                </IconButton>
+              </Grid>
             </Grid>
           </DialogActions>
           <DialogContent className={classes.dialogContent}>
             <Typography variant="body2" style={{ color: "white" }}>
-              <NavigationList navigation={navigationMenu} />
+              <NavigationList
+                navigation={analysisMenu.subnav}
+                onNavigate={handleNavigate}
+                open={openAnalysisMenu}
+              />
             </Typography>
           </DialogContent>
         </Dialog>
       </Grid>
-    </Grid>
+    </Section>
   );
 }
 
