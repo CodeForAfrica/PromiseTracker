f79df7f4d10e363d4265abbfd929cdc49ba49709
nerdlet <brndnyokabi@gmail.com>
2020-09-21 13:07:53 +0300

    Add for mobile

---
+++ src/components/Hero/Chart/MobIleChart/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MobileChart() {
+  return <div> example mobile chart</div>;
+}
+
+export default MobileChart;
--- src/components/Hero/Chart/ProfileChart.js
+++ src/components/Hero/Chart/ProfileChart.js
@@ -1,23 +1,34 @@
 import React from "react";
 
-import { Grid } from "@material-ui/core";
+import { Grid, useMediaQuery } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+
 import PromiseKeptChart from "@/promisetracker/components/Hero/Chart/DesktopChart/PromiseKeptChart";
 import UncertainChart from "@/promisetracker/components/Hero/Chart/DesktopChart/UncertainChart";
 import PromiseNotKeptChart from "@/promisetracker/components/Hero/Chart/DesktopChart/PromiseNotKeptChart";
+import MobileChart from "@/promisetracker/components/Hero/Chart/MobIleChart";
 
-function ProfileChart() {
+function ProfileChart(props) {
+  const theme = useTheme(props);
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   return (
-    <Grid
-      container
-      item
-      direction="row"
-      spacing={2}
-      style={{ padding: "2rem 0rem", position: "relative" }}
-    >
-      <PromiseKeptChart />
-      <UncertainChart />
-      <PromiseNotKeptChart />
-    </Grid>
+    <>
+      {isDesktop ? (
+        <Grid
+          container
+          item
+          direction="row"
+          spacing={2}
+          style={{ padding: "2rem 0rem", position: "relative" }}
+        >
+          <PromiseKeptChart />
+          <UncertainChart />
+          <PromiseNotKeptChart />
+        </Grid>
+      ) : (
+        <MobileChart />
+      )}
+    </>
   );
 }
 
