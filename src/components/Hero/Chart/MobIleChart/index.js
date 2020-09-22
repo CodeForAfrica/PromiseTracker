9a0659db548b05d7e62d0a29a027e9fbff2a7499
nerdlet <brndnyokabi@gmail.com>
2020-09-22 11:51:52 +0300

    Update changes

--- src/components/Hero/Chart/MobIleChart/index.js
+++ src/components/Hero/Chart/MobIleChart/index.js
@@ -3,25 +3,9 @@ import { Grid, Typography } from "@material-ui/core";
 
 function MobileChart() {
   return (
-    <>
-      <Grid container item xs={12} direction="row">
-        <Grid item>
-          <Typography variant="caption">Promise Kept</Typography>
-        </Grid>
-        <Grid item>
-          <svg width={50} height={50}>
-            <circle
-              cx={25}
-              cy={25}
-              r={10}
-              fill="#145BD5"
-              stroke="#1D1D1B"
-              strokeWidth="1"
-            />
-          </svg>
-        </Grid>
-      </Grid>
-    </>
+    <Grid container item xs={12} direction="row">
+      <Typography variant="body2">Mobile chart here</Typography>
+    </Grid>
   );
 }
 
