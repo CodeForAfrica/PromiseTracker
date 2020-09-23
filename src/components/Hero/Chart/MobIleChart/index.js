36fa5ce0145d3ede4e4df75a38bf0e23c6210b70
nerdlet <brndnyokabi@gmail.com>
2020-09-23 21:01:16 +0300

    Mobile svg

--- src/components/Hero/Chart/MobIleChart/index.js
+++ src/components/Hero/Chart/MobIleChart/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -17,7 +17,59 @@ function MobileChart(props) {
       alignItems="flex-start"
       className={classes.root}
     >
-      mobile view
+      <Grid
+        container
+        item
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        <Grid item xs={4}>
+          <Typography variant="caption">Promise Kept</Typography>
+        </Grid>
+
+        <Grid
+          container
+          item
+          xs={9}
+          direction="column"
+          justify="flex-start"
+          alignItems="flex-start"
+          style={{ borderLeft: "1px solid red" }}
+        >
+          <Grid item>
+            <svg width={25} height={25}>
+              <circle
+                cx={12.5}
+                cy={12.5}
+                r={10}
+                fill="blue"
+                stroke="black"
+                strokeWidth="1"
+              />
+            </svg>
+            <Typography variant="caption">130</Typography>
+            <Typography variant="caption">25%</Typography>
+            <Typography variant="caption">completed</Typography>
+          </Grid>
+
+          <Grid item>
+            <svg width={25} height={25}>
+              <circle
+                cx={12.5}
+                cy={12.5}
+                r={10}
+                fill="blue"
+                stroke="black"
+                strokeWidth="1"
+              />
+            </svg>
+            <Typography variant="caption">130</Typography>
+            <Typography variant="caption">25%</Typography>
+            <Typography variant="caption">completed</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
