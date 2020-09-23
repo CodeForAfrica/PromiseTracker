ea13f0a527927c7f1cbd6fc37e3ace17e40cfad8
Obed Amoasi <obedamoasi@gmail.com>
2020-09-23 06:16:14 +0000

    add selected attribute to status

--- src/components/Promises/Status.js
+++ src/components/Promises/Status.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
+import clsx from "clsx";
 
 const useStyles = makeStyles(({ palette }) => ({
   label: {
@@ -12,6 +13,10 @@ const useStyles = makeStyles(({ palette }) => ({
     display: "flex",
     flexWrap: "wrap",
     marginTop: "1rem",
+  },
+  selected: {
+    backgroundColor: palette.primary.dark,
+    color: palette.secondary.light,
   },
   status: {
     marginRight: ".5rem",
@@ -31,8 +36,12 @@ function Status({ statuses, ...props }) {
         Promises by status
       </Typography>
       <div className={classes.root}>
-        {statuses.map((status) => (
-          <Typography key={status.name} variant="h6" className={classes.status}>
+        {statuses.map((status, idx) => (
+          <Typography
+            key={status.name}
+            variant="h6"
+            className={clsx(classes.status, idx === 0 && classes.selected)}
+          >
             {status.name}
           </Typography>
         ))}
