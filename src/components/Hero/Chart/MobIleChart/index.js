cbea784ef965d33926d0555a32f8e1e74f818da4
nerdlet <brndnyokabi@gmail.com>
2020-09-23 11:54:38 +0300

    Remove grids

--- src/components/Hero/Chart/MobIleChart/index.js
+++ src/components/Hero/Chart/MobIleChart/index.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { Grid, Typography, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {},
-  promises: {
-    display: "flex",
-    flexdirection: "row",
-  },
 }));
 
 function MobileChart(props) {
@@ -15,51 +11,13 @@ function MobileChart(props) {
     <Grid
       container
       direction="column"
-      justify="flex-start"
-      alignItems="flex-start"
       item
       xs={12}
-      classes={classes.root}
+      justify="flex-start"
+      alignItems="flex-start"
+      className={classes.root}
     >
-      <Grid container item xs={12} justify="flex-start" alignItem="center">
-        <Grid item xs={3}>
-          <Typography variant="body2">Promise Kept</Typography>
-        </Grid>
-
-        <Grid
-          item
-          xs={8}
-          container
-          direction="row"
-          justify="space-even"
-          alignItems="center"
-        >
-          <Grid item>
-            <svg width={50} height={50}>
-              <circle
-                cx={25}
-                cy={25}
-                r={15}
-                fill="#145BD5"
-                stroke="#1D1D1B"
-                strokeWidth="1"
-              />
-            </svg>
-          </Grid>
-
-          <div>
-            <Typography variant="h4">130</Typography>
-          </div>
-
-          <div>
-            <Typography variant="h4">25%</Typography>
-          </div>
-
-          <div>
-            <Typography variant="h4">completed</Typography>
-          </div>
-        </Grid>
-      </Grid>
+      mobile view
     </Grid>
   );
 }
