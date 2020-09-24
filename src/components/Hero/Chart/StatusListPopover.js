5c1898a723cb0c557bd2243dfe99fed654af6a27
nerdlet <brndnyokabi@gmail.com>
2020-09-24 15:34:30 +0300

    Update statuslistpopover

--- src/components/Hero/Chart/StatusListPopover.js
+++ src/components/Hero/Chart/StatusListPopover.js
@@ -1,58 +1,86 @@
 import React, { useState } from "react";
-
 import PropTypes from "prop-types";
 
 import {
   Grid,
   Button,
   Dialog,
-  IconButton,
-  DialogTitle,
-  DialogContent,
   Typography,
-  useMediaQuery,
+  IconButton,
+  DialogContent,
 } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import CloseIcon from "@material-ui/icons/Close";
-
+import { makeStyles } from "@material-ui/core/styles";
 import info from "@/promisetracker/assets/hero-icon-info.svg";
 
 import config from "@/promisetracker/config";
 
-const useStyles = makeStyles(() => ({
-  root: {},
-  button: {},
-  status: {
-    padding: "1rem",
-    borderBottom: "1px solid #ebebeb",
+const useStyles = makeStyles(({ palette, typography }) => ({
+  typo: {
+    color: palette.primary.main,
   },
+  status: {
+    borderBottom: `1px solid ${palette.secondary.main}`,
+    padding: "0.5rem 0rem",
+  },
+  button: {},
   iconButton: {
-    background: "#F7F7F7",
+    background: palette.secondary.light,
     padding: "1.5rem",
     margin: "0.5rem",
   },
   closeButton: {
     position: "absolute",
     right: "1rem",
-    top: "1rem",
-    color: "grey",
+    top: 0,
+    color: palette.primary.main,
+  },
+  description: {
+    fontSize: typography.pxToRem(10),
   },
 }));
-function StatusListPopover({ onClose, ...props }) {
+
+function DialogTitle({ children, onClose, ...other }) {
+  const classes = useStyles();
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h5" className={classes.typo}>
+        {children}
+      </Typography>
+      {onClose ? (
+        <IconButton
+          disableRipple
+          disableFocusRipple
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+}
+
+DialogTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+function StatusListPopover(props) {
   const classes = useStyles(props);
-  const theme = useTheme();
-  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   const { statuses } = config;
   const [open, setOpen] = useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
+
   return (
-    <div>
+    <>
       <IconButton
         disableRipple
         disableFocusRipple
@@ -63,71 +91,44 @@ function StatusListPopover({ onClose, ...props }) {
       >
         <img src={info} alt="Info" />
       </IconButton>
-      {!isDesktop ? (
-        <Dialog
-          onClose={handleClose}
-          aria-labelledby="customized-dialog-title"
-          open={open}
-        >
-          <DialogTitle
-            disableTypography
-            className={classes.root}
-            onClose={handleClose}
-          >
-            <Typography variant="h3">What do the ratings mean</Typography>
-            <IconButton
-              aria-label="close"
-              className={classes.closeButton}
-              onClick={onClose}
+
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          What do the ratings mean?
+        </DialogTitle>
+        <DialogContent>
+          {statuses.map((status) => (
+            <Grid
+              container
+              direction="row"
+              justify="space-bewtween"
+              alignItems="center"
+              className={classes.status}
             >
-              <CloseIcon />
-            </IconButton>
-          </DialogTitle>
-          <DialogContent>
-            {statuses.map((status) => (
-              <Grid
-                container
-                direction="row"
-                justify="space-bewtween"
-                alignItems="center"
-                className={classes.status}
-              >
-                <Grid item xs={6}>
-                  <Button
-                    variant="contained"
-                    aria-label={status.name}
-                    className={classes.button}
-                  >
-                    {status.name || status}
-                  </Button>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography variant="body2">{status.description}</Typography>
-                </Grid>
+              <Grid item xs={6}>
+                <Button
+                  variant="contained"
+                  aria-label={status.name}
+                  className={classes.button}
+                >
+                  {status.name}
+                </Button>
               </Grid>
-            ))}
-          </DialogContent>
-        </Dialog>
-      ) : (
-        <div>example here</div>
-      )}
-    </div>
+              <Grid item xs={6}>
+                <Typography variant="body2" className={classes.description}>
+                  {status.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          ))}
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
-
-StatusListPopover.propTypes = {
-  name: PropTypes.oneOf([
-    "completed",
-    "in-progress",
-    "stalled",
-    "delayed",
-    "unrated",
-    "unstarted",
-    "",
-    undefined,
-    null,
-  ]).isRequired,
-  onClose: PropTypes.func.isRequired,
-};
 
 export default StatusListPopover;
