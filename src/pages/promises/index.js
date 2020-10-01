f8361a15e8c3eda01850b0a78209fbb683bbc9b0
Clemence Kyara <clemence@codeforafrica.org>
2020-10-01 16:15:24 +0300

    Use various status in promises page

--- src/pages/promises/index.js
+++ src/pages/promises/index.js
@@ -8,6 +8,7 @@ import Promises from "@/promisetracker/components/Promises";
 import Page from "@/promisetracker/components/Page";
 import Subscribe from "@/promisetracker/components/Newsletter";
 
+import config from "@/promisetracker/config";
 import promiseImage from "@/promisetracker/assets/promise-thumb-01.png";
 
 const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
@@ -29,20 +30,18 @@ function Index(props) {
   return (
     <Page title="Promises" classes={{ section: classes.section }}>
       <Promises
-        items={Array(6).fill({
-          date: "2019-08-10",
-          description: `
+        items={Array(6)
+          .fill(null)
+          .map((_, i) => ({
+            date: "2019-08-10",
+            description: `
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               euismod odio non leo pretium pellentesque.
             `,
-          image: promiseImage,
-          status: {
-            color: "#FFB322",
-            textColor: "#202020",
-            title: "delayed",
-          },
-          title: "Codification of national sports and athletics law",
-        })}
+            image: promiseImage,
+            status: config.promiseStatuses[i % config.promiseStatuses.length],
+            title: "Codification of national sports and athletics law",
+          }))}
         title="Promises"
         classes={{
           section: classes.section,
