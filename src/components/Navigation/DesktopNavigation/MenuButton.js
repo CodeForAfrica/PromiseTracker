81c7495027a2f9dd5babcfab9b6a41f117dfeb8e
Obed Amoasi <obedamoasi@gmail.com>
2020-10-01 22:20:44 +0000

    fix menu button display

--- src/components/Navigation/DesktopNavigation/MenuButton.js
+++ src/components/Navigation/DesktopNavigation/MenuButton.js
@@ -5,24 +5,15 @@ import LinkButton from "@/promisetracker/components/Link/Button";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(({ palette, typography }) => ({
+const useStyles = makeStyles(({ typography }) => ({
   root: {
     border: 0,
-    color: palette.primary.main,
     padding: "0.8rem ",
     textTransform: "uppercase",
     fontFamily: typography.fontFamily,
     letterSpacing: "0.56px",
     fontWeight: 600,
     fontSize: typography.pxToRem(14),
-    "&:hover": {
-      border: 0,
-    },
-    "&.active": {
-      backgroundColor: palette.primary.dark,
-      color: "white",
-      borderRadius: 0,
-    },
   },
 }));
 
