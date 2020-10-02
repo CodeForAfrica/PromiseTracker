7846efa390893a4df0e7b9521e6dfc991083e65e
Obed Amoasi <obedamoasi@gmail.com>
2020-10-02 09:13:45 +0000

    fix styles; radar mobile size

--- src/components/Promise/Radar.js
+++ src/components/Promise/Radar.js
@@ -3,15 +3,20 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import radar from "@/promisetracker/assets/promisepage-map-mockup.svg";
 
-const useStyles = makeStyles(({ typography }) => ({
+const useStyles = makeStyles(({ typography, breakpoints }) => ({
   root: {
     alignItems: "center",
     display: "flex",
+    marginBottom: "2rem",
   },
 
   radar: {
-    width: typography.pxToRem(256),
+    width: "100%",
+    objectFit: "cover",
     height: typography.pxToRem(256),
+    [breakpoints.up("lg")]: {
+      width: typography.pxToRem(256),
+    },
   },
 }));
 
