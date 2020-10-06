ef7281a71cd59b0ce187b4491f9baa563f66ccc1
nerdlet <brndnyokabi@gmail.com>
2020-10-06 15:06:42 +0300

    Remove all status

--- src/config.js
+++ src/config.js
@@ -198,43 +198,6 @@ const config = {
       font: "#FFFFFF",
     },
   },
-  allStatus: [
-    {
-      name: "Completed",
-      slug: "completed",
-      description: "The promise is mostly or completely fulfilled.",
-    },
-    {
-      name: "In progress",
-      slug: "in-progress",
-      description: "The promise is in the works or being considered.",
-    },
-    {
-      name: "Stalled",
-      slug: "stalled",
-      description:
-        "Could occur due to inaction by administration or lack of support from legislative branch.",
-    },
-    {
-      name: "Delayed",
-      slug: "delayed",
-      description:
-        "No progress, perhaps due to financial limitations, opposition from lawmakers or a change in priorities.",
-    },
-    {
-      name: "Unrated",
-      slug: "unrated",
-      description:
-        "Every promise begins at this level and retains this rating until evidence of progress or proof that it has been shelved.",
-    },
-    {
-      name: "Unstarted",
-      slug: "unstarted",
-      description:
-        "The promise is accomplished only in part, but has succeeded at least in part consistently with the goal of the promise.",
-    },
-  ],
-
   promiseStatuses: [
     {
       color: "#005DFD",
