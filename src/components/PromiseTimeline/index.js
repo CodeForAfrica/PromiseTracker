97a33f4662edb55b4d93ac9935c51ceaab37d722
Clemence Kyara <clemence@codeforafrica.org>
2020-10-08 21:38:20 +0300

    overflow visible for events + accept arrays

--- src/components/PromiseTimeline/index.js
+++ src/components/PromiseTimeline/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useTheme } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { useMediaQuery } from "@material-ui/core";
 import Labels from "./Labels";
 import PromiseEvent from "./PromiseEvent";
 import PromiseStatus from "./PromiseStatus";
 
-function Index({ duration, events, status }) {
+const useStyles = makeStyles(() => ({
+  root: {
+    overflow: "visible",
+  },
+}));
+
+function Index({ duration, events, statuses, ...props }) {
+  const classes = useStyles(props);
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
+
   return (
     <>
-      <svg width="100%" height="140">
+      <svg width="100%" height="140" className={classes.root}>
         <rect
           x="8"
           y="69"
@@ -22,26 +30,24 @@ function Index({ duration, events, status }) {
           }}
         />
         {isDesktop &&
-          events.map((event) => (
+          events?.map((event) => (
             <>
-              <PromiseEvent
-                key={event.label}
-                duration={duration}
-                event={event}
-              />
+              <PromiseEvent key={event.label} duration={duration} {...event} />
             </>
           ))}
-        <PromiseStatus duration={duration} status={status}>
-          <rect
-            x="8"
-            y="69"
-            width="100%"
-            height="2"
-            style={{
-              fill: "#EBEBEB",
-            }}
-          />
-        </PromiseStatus>
+        {statuses?.map((status) => (
+          <PromiseStatus key={status.title} duration={duration} {...status}>
+            <rect
+              x="8"
+              y="69"
+              width="100%"
+              height="2"
+              style={{
+                fill: "#EBEBEB",
+              }}
+            />
+          </PromiseStatus>
+        ))}
         <Labels duration={duration} />
       </svg>
     </>
@@ -50,23 +56,13 @@ function Index({ duration, events, status }) {
 
 Index.propTypes = {
   duration: PropTypes.arrayOf(PropTypes.number),
-  events: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      year: PropTypes.number,
-      color: PropTypes.string,
-    })
-  ),
-  status: PropTypes.shape({}),
+  events: PropTypes.arrayOf(PropTypes.shape({})),
+  statuses: PropTypes.arrayOf(PropTypes.shape({})),
 };
 Index.defaultProps = {
   duration: [2012, 2018],
-  events: [
-    { year: 2017, label: "Event A", color: "white" },
-    { year: 2015, label: "Event B", color: "white" },
-    { year: 2013, label: "Delayed", color: "#FFB322" },
-  ],
-  status: { year: 2013, label: "Delayed", color: "#FFB322" },
+  events: undefined,
+  statuses: undefined,
 };
 
 export default Index;
