70770767597f8c8e9d63ab377de7fa029622964d
Clemence Kyara <clemence@codeforafrica.org>
2020-10-09 09:21:42 +0300

    Use props + style fixes

--- src/components/Hero/Profile/index.js
+++ src/components/Hero/Profile/index.js
@@ -7,21 +7,22 @@ import { Grid, Typography } from "@material-ui/core";
 
 import profilePic from "@/promisetracker/assets/hero-sonko.png";
 
-const useStyles = makeStyles(({ breakpoints, palette }) => ({
+const useStyles = makeStyles(({ breakpoints, palette, typography }) => ({
   root: {
     [breakpoints.up("lg")]: {
-      marginLeft: "-5rem",
+      marginLeft: typography.pxToRem(-55),
     },
   },
   caption: {
     color: "#20202059",
   },
   img: {
-    width: "149px",
-    height: "149px",
+    maxWidth: typography.pxToRem(149),
+    objectFit: "cover",
+    width: typography.pxToRem(149),
     [breakpoints.up("lg")]: {
-      width: "100%",
-      height: "auto",
+      maxWidth: typography.pxToRem(350),
+      width: typography.pxToRem(350),
     },
   },
   subtitle: {
@@ -33,24 +34,16 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
   },
 }));
 
-function Profile({ subtitle, name, date, ...props }) {
+function Profile({ title, name, date, ...props }) {
   const classes = useStyles(props);
   return (
-    <Grid
-      container
-      direction="row"
-      alignItems="center"
-      item
-      xs={12}
-      lg={4}
-      className={classes.root}
-    >
+    <Grid container alignItems="center" className={classes.root}>
       <Grid item xs={6} lg={12}>
-        <img src={profilePic} alt="Profile" className={classes.img} />
+        <img src={profilePic} alt={name} className={classes.img} />
       </Grid>
       <Grid item xs={6} lg={12}>
         <Typography variant="h5" className={classes.subtitle}>
-          {subtitle}
+          {title}
         </Typography>
         <Typography variant="h6" className={classes.caption}>
           {date}
@@ -61,7 +54,7 @@ function Profile({ subtitle, name, date, ...props }) {
 }
 
 Profile.propTypes = {
-  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
