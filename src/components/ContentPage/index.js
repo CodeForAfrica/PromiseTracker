72f48e1bdfc8003ba467a9b5cade7a6e496b2f67
Clemence Kyara <clemence@codeforafrica.org>
2020-10-12 12:02:20 +0300

    Rerender on slug change + add docs

--- src/components/ContentPage/index.js
+++ src/components/ContentPage/index.js
@@ -10,19 +10,27 @@ import Page from "@/promisetracker/components/Page";
 
 import useStyles from "./useStyles";
 
-function ContentPage({ aside, children, content, title, ...props }) {
+/**
+ * ContentPage is used to render related pages usually from CMS.
+ *
+ * Presence of `slug` is used by `ContentPage` to indicate a page has changed.
+ * For example, from `/about/methodology` with slug `methodology` to
+ * `/about/partners` with slug `partners`
+ */
+function ContentPage({ aside, children, content, slug, title, ...props }) {
   const classes = useStyles(props);
 
   return (
     <Page
       {...props}
+      key={slug}
       title={title}
       classes={{ section: classes.section, footer: classes.footer }}
     >
       <Section
         title={title}
-        classes={{ root: classes.section, title: classes.sectionTitle }}
         titleProps={{ component: H1 }}
+        classes={{ root: classes.section, title: classes.sectionTitle }}
       >
         <Grid container justify="space-between" className={classes.grid}>
           <Grid item xs={12} lg={7} className={classes.gridMain}>
@@ -44,12 +52,14 @@ ContentPage.propTypes = {
   aside: PropTypes.node,
   children: PropTypes.node,
   content: PropTypes.node.isRequired,
+  slug: PropTypes.string,
   title: PropTypes.string,
 };
 
 ContentPage.defaultProps = {
   aside: undefined,
   children: undefined,
+  slug: undefined,
   title: undefined,
 };
 
