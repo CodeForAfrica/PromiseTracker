460c2d48e5c557b856542506934b07dfca1cb5aa
Clemence Kyara <clemence@codeforafrica.org>
2020-10-12 09:30:53 +0300

    Fix section styling

--- src/components/AboutPage/useStyles.js
+++ src/components/AboutPage/useStyles.js
@@ -1,7 +1,16 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(({ breakpoints, typography }) => ({
-  section: {},
+const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
+  section: {
+    padding: `0 ${typography.pxToRem(23)}`,
+    margin: 0,
+    width: "100%",
+    [breakpoints.up("lg")]: {
+      padding: 0,
+      margin: "0 auto",
+      width: typography.pxToRem(widths.values.lg),
+    },
+  },
   sectionTitle: {},
   actNow: {
     marginTop: typography.pxToRem(52),
