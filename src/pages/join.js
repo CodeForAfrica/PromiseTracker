82d124c916cd729434d29f699eaa37e0a028304d
Clemence Kyara <clemence@codeforafrica.org>
2020-10-13 12:05:30 +0300

    New join us page per design

---
+++ src/pages/join.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import { RichTypography } from "@commons-ui/core";
+
+import ActNow from "@/promisetracker/components/ActNow";
+import ContentPage from "@/promisetracker/components/ContentPage";
+
+const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
+  section: {
+    padding: `0 ${typography.pxToRem(23)}`,
+    margin: 0,
+    width: "100%",
+    [breakpoints.up("lg")]: {
+      padding: 0,
+      margin: "0 auto",
+      width: typography.pxToRem(widths.values.lg),
+    },
+  },
+  description: {
+    marginBottom: typography.pxToRem(40),
+    [breakpoints.up("lg")]: {
+      marginBottom: typography.pxToRem(86),
+    },
+  },
+  footer: {
+    marginTop: 0,
+  },
+}));
+
+function Index(props) {
+  const classes = useStyles(props);
+
+  return (
+    <ContentPage
+      slug="join"
+      title="Join us"
+      classes={{
+        section: classes.section,
+        footer: classes.footer,
+      }}
+      content={
+        <RichTypography className={classes.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip.
+        </RichTypography>
+      }
+    >
+      <ActNow classes={{ section: classes.section }} />
+    </ContentPage>
+  );
+}
+
+export default Index;
