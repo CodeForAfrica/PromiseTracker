7b695d3a37b0a946237c3611e93b31f799b82366
Clemence Kyara <clemence@codeforafrica.org>
2020-10-19 18:32:29 +0300

    Remove apollo from _app; We'll access via Page data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { ApolloProvider } from "@apollo/client";
-
 import { DefaultSeo } from "next-seo";
 
 import { ThemeProvider } from "@material-ui/core/styles";
@@ -10,13 +8,11 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 
 import theme from "@/promisetracker/theme/index";
 import SEO from "next-seo.config";
-import createApolloClient from "@/promisetracker/lib/createApolloClient";
 
 import "simplebar/dist/simplebar.css";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-  const apolloClient = createApolloClient();
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
@@ -26,14 +22,14 @@ export default function MyApp(props) {
   }, []);
 
   return (
-    <ApolloProvider client={apolloClient}>
+    <>
       <DefaultSeo {...SEO} />
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </ApolloProvider>
+    </>
   );
 }
 
