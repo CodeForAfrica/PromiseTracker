07a6538255fb4f18a2aae79a3b21cbb5542bc901
Clemence Kyara <clemence@codeforafrica.org>
2020-10-19 18:11:05 +0300

    Add team + clean up

--- src/lib/createApolloClient.js
+++ src/lib/createApolloClient.js
@@ -1,17 +1,20 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
 import config from "@/promisetracker/config";
 
-export default function createApolloClient(initialState = {}) {
+export default function createApolloClient(initialState = {}, team) {
+  const { GRAPHQL_URI } = config;
+  const uri = team ? `${GRAPHQL_URI}?team=${team}` : GRAPHQL_URI;
+
   return new ApolloClient({
     ssrMode: typeof window === "undefined", // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri: config.GRAPHQL_URI, // Server URL (must be absolute)
-      // credentials: 'include', // Additional fetch() options like `credentials` or `headers`
       headers: {
         "X-Check-Token": process.env.CHECK_ACCESS_TOKEN,
       },
-      fetch,
+      uri,
     }),
     cache: new InMemoryCache().restore(initialState),
   });
