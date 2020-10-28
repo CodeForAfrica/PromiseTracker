56fb12356f0a8da52be3c5880a8c33e0d601f106
Clemence Kyara <clemence@codeforafrica.org>
2020-10-28 16:21:50 +0300

    Remove use of server-side envs in config

--- src/config.js
+++ src/config.js
@@ -17,20 +17,15 @@ import partner6 from "@/promisetracker/assets/partner-logo_5@2x.png";
 
 import logo from "@/promisetracker/assets/logo-C4A.svg";
 
-const WP_BACKEND_URL =
-  process.env.WP_BACKEND_URL || "https://dashboard.hurumap.org/promisetracker";
-
 const config = {
-  WP_BACKEND_URL,
-  WP_HURUMAP_DATA_API: `${WP_BACKEND_URL}/wp-json/hurumap-data`,
   DEFAULT_LANG: "en",
-
-  title: "PromiseTracker",
-  url: "https://promisetracker.pesacheck.org/",
   GRAPHQL_URI: "https://check-api.checkmedia.org/api/graphql",
   PROXY_URL: "https://corsanywhere.devops.codeforafrica.org/",
   CHECK_PROJECT_DB_ID: 2831,
   CHECK_PROMISE_MAX_COUNT: 150,
+  WP_DASHBOARD_URL: "https://dashboard.hurumap.org/promisetracker",
+  title: "PromiseTracker",
+  url: "https://promisetracker.pesacheck.org/",
   settings: {
     subscribe: {
       embedCode: `
