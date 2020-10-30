adb7e7974ff25396656f0b357fb6c6e1faae187a
Obed Amoasi <obed@codeforafrica.org>
2020-10-30 14:08:47 +0000

    adapt, promise object to project

--- src/lib/check/index.js
+++ src/lib/check/index.js
@@ -3,6 +3,8 @@ import {
   GET_PROMISES_BY_CATEGORIES,
 } from "@/promisetracker/lib/check/gql";
 
+import config from "@/promisetracker/config";
+import promiseImage from "@/promisetracker/assets/promise-thumb-01.png";
 import createApolloClient from "./createApolloClient";
 
 const UNSPECIFIED_TEAM = "unspecified";
@@ -16,10 +18,61 @@ function check(team = undefined, initialState = {}) {
     CLIENT_PER_TEAM.set(clientTeam, existingClient);
   }
 
+  function getTaskfromTasks(tasks, label) {
+    return tasks.find((item) => item.node.label === label);
+  }
+
+  function findLogByTask(logs, label) {
+    return logs.find((item) => item.node.task?.label === label);
+  }
+
+  function getassetURL(filename, id) {
+    return `${config.CHECK_ASSET_URI}/${id}/${filename.replace(/ /g, "_")}`;
+  }
+
+  function getStatus(tasks) {
+    const statusTask = getTaskfromTasks(
+      tasks,
+      "What is the status of the promise?"
+    );
+    const promiseStatus = config.promiseStatuses.find(
+      (status) => status.title === statusTask.node.first_response_value
+    );
+    const defaultStatus = config.promiseStatuses.find(
+      (status) => status.title === "Unrated"
+    );
+    return promiseStatus || defaultStatus;
+  }
+
+  function getImage(logs, tasks) {
+    const imageLog = findLogByTask(
+      logs,
+      "What is the image related to the promise"
+    );
+    const imageTask = getTaskfromTasks(
+      tasks,
+      "What is the image related to the promise"
+    );
+    const filename = imageTask.node.first_response_value;
+    const annotationChanges = JSON.parse(imageLog.node.object_changes_json)
+      .annotation_id;
+    const id = annotationChanges ? annotationChanges[1] : null; // latest image ID
+    return filename ? getassetURL(filename, id) : promiseImage;
+  }
+
   const api = {
     promises: async (variables) => {
       return client.query({ query: GET_PROMISES, variables }).then((res) => {
-        return res.data.search.medias.edges.map((promise) => promise.node);
+        return res.data.search.medias.edges.map(({ node }) => ({
+          id: node.id,
+          title: node.title,
+          image: getImage(node.log?.edges, node.tasks?.edges),
+          description: node.description,
+          date: new Date(parseInt(node.created_at, 10)).toDateString({
+            dateStyle: "short",
+          }),
+          status: getStatus(node.tasks?.edges),
+        }));
       });
     },
     promisesByCategories: async (variables) => {
