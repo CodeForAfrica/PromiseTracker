de25743ad14f6cfcc6a2eb0e9e4b365b06c599c0
Clemence Kyara <clemence@codeforafrica.org>
2020-11-11 09:33:45 +0300

    Add languageAlternates & switch to using server

--- src/lib/i18n/index.js
+++ src/lib/i18n/index.js
@@ -1,24 +1,47 @@
 import config from "@/promisetracker/config";
+import server from "@/promisetracker/lib/server";
 
 function i18n(site) {
-  const SITE = site?.length ? `${site.trim().toUpperCase()}_` : "";
-  const DEFAULT_LOCALE =
-    process.env[`${SITE}DEFAULT_LOCALE`] || config.DEFAULT_LOCALE;
-  const LOCALES = process.env[`${SITE}LOCALES`] || config.LOCALES;
+  const siteServer = server(site);
 
   const api = {
     get defaultLocale() {
-      return DEFAULT_LOCALE;
+      return siteServer.defaultLocale;
     },
     get locales() {
-      return LOCALES;
+      return siteServer.locales;
     },
 
     language: (locale) => config.LANGUAGE_BY_LOCALE[locale] || locale,
 
+    /**
+     * .
+     * languageAlternates is not the best name in the world but
+     * (Next Seo)[https://github.com/garmeeh/next-seo#nextseo-options]
+     * uses it so we'll use it here as well.
+     */
+    languageAlternates: (asPath = "") => {
+      const { url } = siteServer;
+      const baseUrl = url.endsWith("/") ? url.slice(0, -1) : url;
+      let pagePath = asPath.endsWith("/") ? asPath.slice(0, -1) : asPath;
+      pagePath =
+        pagePath.length && !pagePath.startsWith("/")
+          ? `/${pagePath}`
+          : pagePath;
+      const alternatives = siteServer.locales.map((locale) => ({
+        hrefLang: locale,
+        href: `${baseUrl}/${locale}${pagePath}`,
+      }));
+      alternatives.push({
+        hrefLang: "x-default",
+        href: `${baseUrl}${pagePath}`,
+      });
+      return alternatives;
+    },
+
     localizePaths: (unlocalizedPaths) => {
       const paths = [];
-      LOCALES.forEach((locale) => {
+      siteServer.locales.forEach((locale) => {
         unlocalizedPaths.forEach((path) => paths.push({ ...path, locale }));
       });
       return paths;
