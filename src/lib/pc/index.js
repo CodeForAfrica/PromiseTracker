dd441543b4cfbc082d8e9ff046dc7ee48ca43a07
Obed Amoasi <obedamoasi@gmail.com>
2020-11-25 13:57:46 +0000

    refactor; handle fetch fail, remove redundant variables

--- src/lib/pc/index.js
+++ src/lib/pc/index.js
@@ -2,8 +2,6 @@ import config from "@/promisetracker/config";
 import server from "@/promisetracker/lib/server";
 import { formatDate } from "@/promisetracker/utils";
 
-
-
 /**
  * Load fact checks stories from PesaCheck site.
  */
@@ -14,13 +12,13 @@ function pc(site) {
   const SITE_FACT_CHECKS_TAG =
     process.env[`${siteServer.site}FACT_CHECKS_TAG`] || config.FACT_CHECKS_TAG;
 
-    const toFactCheck = (report) => ({
-      date: formatDate(report.updatedAt) || null,
-      image: `https://cdn-images-1.medium.com/max/480/${report.virtuals.previewImage?.imageId}`,
-      description: report.virtuals?.subtitle || null,
-      href: `${SITE_FACT_CHECKS_URL}/${report.uniqueSlug}`,
-      title: report.title || null,
-    });
+  const toFactCheck = (report) => ({
+    date: formatDate(report.updatedAt) || null,
+    image: `https://cdn-images-1.medium.com/max/480/${report.virtuals.previewImage?.imageId}`,
+    description: report.virtuals?.subtitle || null,
+    href: `${SITE_FACT_CHECKS_URL}/${report.uniqueSlug}`,
+    title: report.title || null,
+  });
 
   async function getFactChecks() {
     const url = `https://corsio.devops.codeforafrica.org/?${SITE_FACT_CHECKS_URL}/tagged/${SITE_FACT_CHECKS_TAG}?format=json`;
@@ -34,50 +32,50 @@ function pc(site) {
         reportStreamItem.postPreview &&
         json.payload.references.Post[reportStreamItem.postPreview.postId]
     );
- 
 
     // Remove null stories & return props supported in PostCard
     return reports.filter((report) => report).map(toFactCheck);
   }
 
-  async function getFactCheckList(urls){
-    return Promise.all(urls?.map(async (url) => getFactCheck(url)) || [] )
-  }
   async function getFactCheck(pesacheckUrl) {
     const url = `https://corsio.devops.codeforafrica.org/?${pesacheckUrl}?format=json`;
     const response = await fetch(url);
     const json = await JSON.parse(
       (await response.text()).replace("])}while(1);</x>", "")
     );
-    return toFactCheck({...json.payload,...json.payload.value})
+    return toFactCheck(json.payload.value);
   }
 
+  async function getFactCheckList(urls) {
+    return Promise.all(urls?.map(async (url) => getFactCheck(url)) || []);
+  }
   const api = {
-    factChecks: (options) => ({
-      get latest() {
-        return (async () => {
-          return getFactChecks();
-        })();
-      },
-      get list() {
-        if(options.urls){
+    factChecks: (options) => {
+      const url = options?.url || options?.urls?.[0];
+      return {
+        get latest() {
           return (async () => {
-            return getFactCheckList(options.urls);
+            return getFactChecks();
           })();
-        }else{
-          throw new Error("URl Array Required")
-        }
-      },
-      get first() {
-        if(options.url){
-          return (async () => {
-            return getFactCheck(options.url);
-          })();
-        }else{
-          throw new Error("URl Required")
-        }
-      }
-    }),
+        },
+        get list() {
+          if (options.urls) {
+            return (async () => {
+              return getFactCheckList(options.urls);
+            })();
+          }
+          return [];
+        },
+        get first() {
+          if (url) {
+            return (async () => {
+              return getFactCheck(url);
+            })();
+          }
+          return null;
+        },
+      };
+    },
   };
   return api;
 }
