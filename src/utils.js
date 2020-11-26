7c9f8ec42570686b06f2a1b20599c25dfc728fb7
Obed Amoasi <obedamoasi@gmail.com>
2020-11-26 12:42:18 +0000

    add util function getlinkfromhref

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import config from "@/promisetracker/config";
+
 export function slugify(string) {
   const a =
     "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
@@ -40,4 +42,8 @@ export function replaceAll(str, mapObj) {
   const re = new RegExp(Object.keys(mapObj).join("|"), "gi");
 
   return str.replace(re, (matched) => mapObj[matched.toLowerCase()]);
+}
+
+export function getLinkFromHref(href) {
+  return config.URL + (href || "");
 }
