2a04f65c82f31ddc66d82302c050d2fa350f9fbf
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-02-16 12:45:57 +0300

    remove config

--- commonsConfig.js
+++
@@ -1,7 +0,0 @@
-const fs = require("graceful-fs");
-const packageOne = require("./package.json");
-const packageTwo = require("./src/commons-ui/package.json");
-
-Object.assign(packageOne.devDependencies, packageTwo.devDependencies);
-
-fs.writeFileSync("./commonsPackage.json", JSON.stringify(packageOne, null, 2));
