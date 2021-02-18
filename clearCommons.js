7f57b9ce9fae6ba43a6892d5e6b4a1ec827f2a9a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-02-18 12:36:02 +0300

    add more config scripts

---
+++ clearCommons.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const babel = require("./.babelrc.json");
+
+if (babel.plugins[1][1].alias["@commons-ui/core"]) {
+  delete babel.plugins[1][1].alias["@commons-ui/core"];
+}
+
+fs.writeFileSync("./.babelrc.json", JSON.stringify(babel, null, 2));
--- commons.js
+++ commons.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
+
 const packageOne = require("./package.json");
 const packageTwo = require("./commons-ui/package.json");
+const babel = require("./.babelrc.json");
 
+// Merge dependencies for install
 Object.assign(packageOne.devDependencies, packageTwo.devDependencies);
 Object.assign(packageOne.devDependencies, packageOne.dependencies);
 packageOne.dependencies = {};
 
+// add alias to babel config file
+Object.assign(babel.plugins[1][1].alias, {
+  "@commons-ui/core": "./commons-ui/packages/core/src",
+});
+
+// update with changes
 fs.writeFileSync("./package.json", JSON.stringify(packageOne, null, 2));
+fs.writeFileSync("./.babelrc.json", JSON.stringify(babel, null, 2));
