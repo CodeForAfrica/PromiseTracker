49decccdfbf89793ce7766f31f6084f1f1bfa4d8
nerdlet <brndnyokabi@gmail.com>
2021-02-25 14:26:45 +0300

    Update preview.js

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -2,10 +2,9 @@ import wp from "@/promisetracker/lib/wp";
 
 export default async function preview(req, res) {
   const { slug } = req.query;
-
   // Fetch WordPress to check if the provided `id` or `slug` exists
   const wpApi = wp();
-  const post = await wpApi.posts({ slug }).posts;
+  const post = await wpApi.posts({ slug }).first;
 
   // If the post doesn't exist prevent preview mode from being enabled
   if (!post) {
@@ -18,8 +17,8 @@ export default async function preview(req, res) {
   });
 
   // Redirect to the path from the fetched post
-  // We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/analyis/articles/${post.slug}` });
+  res.redirect(`/analysis/articles/${post.slug}`);
   res.end();
+
   return post;
 }
