e8e87c32a34528a11599161e03c61667ffb7e19e
nerdlet <brndnyokabi@gmail.com>
2021-02-25 08:37:06 +0300

    Add preview for post

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,4 +1,33 @@
-export default function handler(req, res) {
+import wp from "@/promisetracker/lib/wp";
+
+export default async function preview(req, res) {
+  const { id, slug } = req.query;
+
+  // Fetch WordPress to check if the provided `id` or `slug` exists
+  const wpApi = wp();
+  const post = await wpApi.pages(id || slug, id || slug).posts;
+
+  // If the post doesn't exist prevent preview mode from being enabled
+  if (!post) {
+    return res.status(401).json({ message: "Post not found" });
+  }
+
+  res.setPreviewData({
+    post: {
+      id: post.id,
+      slug: post.slug,
+    },
+  });
+
+  // Redirect to the path from the fetched post
+  // We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
+  res.writeHead(307, { Location: `/posts/${post.slug || post.id}` });
+  res.end();
+  return post;
+}
+
+/* export default function handler(req, res) {
   res.setPreviewData({});
   res.end("Preview mode enabled");
-}
+} 
+ */
