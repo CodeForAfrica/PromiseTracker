8582de9e7695d04093a9cb9c480b242276a025a8
Obed Amoasi <obedamoasi@gmail.com>
2021-03-10 23:04:49 +0000

    defer post fetching

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,36 +1,19 @@
-import wp from "@/promisetracker/lib/wp";
-
 export default async function preview(req, res) {
   const {
     postType,
     postId,
     revisionId,
-    _wpnonce: nonce,
     _thumbnail_id: thumbnailId,
   } = req.query;
-  // Fetch WordPress to check if the provided `id` or `slug` exists
-  const wpApi = wp();
-  let post;
-  if (postType === "pages") {
-    post = await wpApi.revisions({ id: postId, nonce, revisionId, thumbnailId })
-      .page;
-  } else if (postType === "posts") {
-    post = await wpApi.revisions({ id: postId, nonce, revisionId, thumbnailId })
-      .post;
-  }
-  // If the post doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(404).redirect(`/not-found`);
-  }
+
   res.setPreviewData({
-    query: { id: postId, nonce, revisionId, thumbnailId },
+    query: { id: postId, revisionId, thumbnailId },
   });
 
   if (postType === "pages") {
-    res.redirect(`/about/${post.slug}`);
+    res.redirect(`/about/${postId}`);
   } else {
-    res.redirect(`/analysis/articles/${post.slug}`);
+    res.redirect(`/analysis/articles/${postId}`);
   }
   res.end();
-  return post;
 }
