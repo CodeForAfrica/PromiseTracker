d302a5176a7ebd9b4ef5d03ba3171ccf99f4846b
_ Kilemensi <clemence@codeforafrica.org>
2021-04-06 16:32:48 +0300

    Start testing Webpack 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withImages = require("next-images");
 
 module.exports = withImages({
+  future: {
+    webpack5: true,
+  },
   i18n: {
     locales: ["am", "ar", "en", "fr"],
     defaultLocale: "en",
