712777c0d26f479bd18766395a35b04d9a7cf996
_ Kilemensi <clemence@codeforafrica.org>
2021-06-18 21:19:25 +0300

    Configre Sentry & next/Image

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withImages = require("next-images");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withImages({
-  future: {
-    webpack5: true,
-  },
+const moduleExports = {
   i18n: {
     locales: ["am", "ar", "en", "fr"],
     defaultLocale: "en",
+  },
+  images: {
+    domains: process.env.IMAGE_DOMAINS?.split(",") ?? "dashboard.hurumap.org",
   },
   async redirects() {
     return [
@@ -17,4 +17,18 @@ module.exports = withImages({
       },
     ];
   },
-});
+};
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
