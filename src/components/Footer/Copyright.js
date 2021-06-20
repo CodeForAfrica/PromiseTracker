e06d11983193ae7aae6d9f47bb4696f9d0a98e77
_ Kilemensi <clemence@codeforafrica.org>
2021-06-20 13:19:24 +0300

    Add copyright component with next/image

---
+++ src/components/Footer/Copyright.js
@@ -0,0 +1,41 @@
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Image from "next/image";
+import PropTypes from "prop-types";
+import React from "react";
+
+const useStyles = makeStyles(({ typography }) => ({
+  root: {},
+  text: {
+    marginLeft: typography.pxToRem(11),
+  },
+  image: {},
+}));
+
+function Copyright({ children, typographyProps, ...props }) {
+  const classes = useStyles(props);
+
+  return (
+    <div className={classes.root}>
+      <Image {...props} className={classes.image} />
+      {children && (
+        <Typography {...typographyProps} className={classes.text}>
+          {children}
+        </Typography>
+      )}
+    </div>
+  );
+}
+
+Copyright.propTypes = {
+  children: PropTypes.node,
+  typographyProps: PropTypes.shape({}),
+};
+
+Copyright.defaultProps = {
+  children: undefined,
+  typographyProps: {
+    variant: "button",
+  },
+};
+export default Copyright;
