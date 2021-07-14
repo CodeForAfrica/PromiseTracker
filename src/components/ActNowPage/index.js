1e39a2443d5ac214b290c9945a27fd3093096e12
_ Kilemensi <clemence@codeforafrica.org>
2021-07-14 13:14:33 +0300

    Use registration dialog

--- src/components/ActNowPage/index.js
+++ src/components/ActNowPage/index.js
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import { signIn } from "next-auth/client";
 import Image from "next/image";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 
 import useStyles from "./useStyles";
 
@@ -12,9 +12,53 @@ import actNowLogo from "@/promisetracker/assets/Component 121 – 1@2x.png";
 import actNowImg from "@/promisetracker/assets/illo-aboutTheProject@2x.png";
 import ContentPage from "@/promisetracker/components/ContentPage";
 import ContentSection from "@/promisetracker/components/ContentPage/Section";
+import IndividualRegistrationDialog from "@/promisetracker/components/IndividualRegistrationDialog";
 
-function ActNow({ footer, title, navigation, description, ...props }) {
+const individualRegistrationDialogArgs = {
+  title: "Individual registration form",
+  fields: {
+    agree: {
+      label: "Agree to our terms and conditions",
+      error: "You must agree to our terms and conditions to continue",
+    },
+    bio: {
+      label: "About me / bio",
+      placeholder: "",
+    },
+    email: {
+      label: "Email*",
+      placeholder: "",
+      error: "Required and must be a valid email address",
+    },
+    firstName: { label: "First name*", placeholder: "", error: "Required" },
+    lastName: { label: "Last name*", placeholder: "", error: "Required" },
+    location: {
+      label: "Location (County, & Town, City, Country)*",
+      placeholder: "",
+      error: "Required",
+    },
+    password: {
+      label: "Password*",
+      placeholder: "",
+      error: "Required",
+    },
+    phoneNumber: { label: "Phone number", placeholder: "" },
+    socialMedia: { label: "Social media link", placeholder: "" },
+    submit: { label: "Submit" },
+  },
+};
+
+function ActNow({ actNow, footer, title, navigation, description, ...props }) {
   const classes = useStyles(props);
+  const [open, setOpen] = useState(false);
+  const submitUrl = actNow?.url;
+
+  const handleRegister = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const aside = (
     <>
@@ -29,80 +73,100 @@ function ActNow({ footer, title, navigation, description, ...props }) {
     </>
   );
   return (
-    <ContentPage
-      {...props}
-      footer={footer}
-      navigation={navigation}
-      title={title}
-      content={
-        <>
-          <figure className={classes.logoFigure}>
-            <Image
-              src={actNowLogo}
-              layout="fill"
-              alt="actNOW"
-              className={classes.image}
-            />
-          </figure>
-          {description?.length && (
-            <RichTypography variant="body2" className={classes.description}>
-              {description}
-            </RichTypography>
-          )}
-        </>
-      }
-      contentProps={{ lg: 7 }}
-      showTitle={false}
-      aside={aside}
-      asideProps={{ lg: 3 }}
-      classes={{
-        section: classes.section,
-        sectionTitle: classes.sectionTitle,
-        footer: classes.footer,
-        grid: classes.grid,
-        gridAside: classes.gridAside,
-        gridContent: classes.gridContent,
-      }}
-    >
-      <div className={classes.contentSection}>
-        <ContentSection
-          contentProps={{ lg: 9, title: null }}
-          aside={
-            <Grid container classes={{ root: classes.imageContainer }}>
-              <Grid item xs={12}>
-                <RichTypography variant="h4">NEW USER</RichTypography>
+    <>
+      <ContentPage
+        {...props}
+        footer={footer}
+        navigation={navigation}
+        title={title}
+        content={
+          <>
+            <figure className={classes.logoFigure}>
+              <Image
+                src={actNowLogo}
+                layout="fill"
+                alt="actNOW"
+                className={classes.image}
+              />
+            </figure>
+            {description?.length && (
+              <RichTypography variant="body2" className={classes.description}>
+                {description}
+              </RichTypography>
+            )}
+          </>
+        }
+        contentProps={{ lg: 7 }}
+        showTitle={false}
+        aside={aside}
+        asideProps={{ lg: 3 }}
+        classes={{
+          section: classes.section,
+          sectionTitle: classes.sectionTitle,
+          footer: classes.footer,
+          grid: classes.grid,
+          gridAside: classes.gridAside,
+          gridContent: classes.gridContent,
+        }}
+      >
+        <div className={classes.contentSection}>
+          <ContentSection
+            contentProps={{ lg: 9, title: null }}
+            aside={
+              <Grid container classes={{ root: classes.imageContainer }}>
+                <Grid item xs={12}>
+                  <RichTypography variant="h4">NEW USER</RichTypography>
+                </Grid>
+                <Grid item xs={12}>
+                  <RichTypography variant="body2">
+                    Create an account here
+                  </RichTypography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    onClick={() => signIn("google")}
+                    className={clsx(classes.authButton, classes.authGoogle)}
+                  >
+                    Continue with Google
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    onClick={handleRegister}
+                    className={clsx(classes.authButton)}
+                  >
+                    Register
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <RichTypography variant="body2">
-                  Create an account here
-                </RichTypography>
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  onClick={() => signIn("google")}
-                  className={clsx(classes.authButton, classes.authGoogle)}
-                >
-                  Continue with Google
-                </Button>
-              </Grid>
-            </Grid>
-          }
-          asideProps={{ lg: 3 }}
-          classes={{
-            section: classes.section,
-            grid: clsx(classes.grid, classes.contentSectionGrid),
-            gridAside: classes.gridAside,
-            gridContent: classes.gridContent,
-          }}
-        />
-      </div>
-    </ContentPage>
+            }
+            asideProps={{ lg: 3 }}
+            classes={{
+              section: classes.section,
+              grid: clsx(classes.grid, classes.contentSectionGrid),
+              gridAside: classes.gridAside,
+              gridContent: classes.gridContent,
+            }}
+          />
+        </div>
+      </ContentPage>
+      <IndividualRegistrationDialog
+        {...individualRegistrationDialogArgs}
+        key={open}
+        open={open}
+        onClose={handleClose}
+        submitUrl={submitUrl}
+      />
+    </>
   );
 }
 
 ActNow.propTypes = {
-  actNow: PropTypes.shape({}),
+  actNow: PropTypes.shape({
+    url: PropTypes.string,
+  }),
   description: PropTypes.string,
   title: PropTypes.string,
   footer: PropTypes.shape({}),
