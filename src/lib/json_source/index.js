34615f9fabf2ad7cef57b7fe2d3cce67a7c9498f
esir <esirkings@gmail.com>
2021-08-03 15:33:21 +0300

    Adds functions to further process promises

--- src/lib/json_source/index.js
+++ src/lib/json_source/index.js
@@ -2,12 +2,22 @@ import JsonSourceClient from "./json_source_client";
 
 const client = JsonSourceClient();
 
+async function handleSinglePromise(promise) {
+  // Perform more actions on the promise here. E.g Getting related promises instead of having them in the json file.
+  return promise;
+}
+
+async function handlePromises(promises) {
+  // Perform more actions on the promises here
+  return promises;
+}
+
 const promiseSource = () => {
   const api = {
     promises: async ({ limit }) => {
-      return client.query({ query: "GET_PROMISES", limit }).then((response) => {
-        return response;
-      });
+      return client
+        .query({ query: "GET_PROMISES", limit })
+        .then(handlePromises);
     },
     promisesByCategories: async (category) => {
       return client
@@ -22,9 +32,9 @@ const promiseSource = () => {
       };
     },
     promise: async ({ id }) => {
-      return client.query({ query: "GET_PROMISE", id }).then((response) => {
-        return response;
-      });
+      return client
+        .query({ query: "GET_PROMISE", id })
+        .then(handleSinglePromise);
     },
   };
   return api;
