3ef4084038bdfba61fb6958f7a5cb86202fea712
_ Kilemensi <clemence@codeforafrica.org>
2021-08-25 15:57:17 +0300

    Initial use of cache in gsheets

--- src/lib/gsheets/index.js
+++ src/lib/gsheets/index.js
@@ -3,15 +3,16 @@ import { camelCase } from "lodash";
 import papa from "papaparse";
 
 // import config from "@/promisetracker/config";
+import cache from "@/promisetracker/lib/cache";
 import server from "@/promisetracker/lib/server";
 // import { formatDate } from "@/promisetracker/utils";
 
-function gsheets(site) {
-  const siteServer = server(site);
+function gsheets(siteSlug) {
+  const siteServer = server(siteSlug);
   const SPREADSHEETS_URL = "https://docs.google.com/spreadsheets/";
   const spreadsheetId = process.env[`${siteServer.site}GSHEETS_SPREADSHEET_ID`];
-  // const entitiesSheetId =
-  //   process.env[`${siteServer.site}GSHEETS_ENTITIES_SHEET_ID`];
+  const entitiesSheetId =
+    process.env[`${siteServer.site}GSHEETS_ENTITIES_SHEET_ID`];
   const sitesSheetId = process.env[`${siteServer.site}GSHEETS_SITES_SHEET_ID`];
   const papaOptions = {
     header: true,
@@ -35,7 +36,7 @@ function gsheets(site) {
     });
   };
 
-  const getSites = async () => {
+  const fetchSitesSheet = async () => {
     const options = {
       ...papaOptions,
       transformHeader: (header) => camelCase(header.trim()),
@@ -43,15 +44,46 @@ function gsheets(site) {
     const response = await fetch(
       `${SPREADSHEETS_URL}d/${spreadsheetId}/export?format=csv&gid=${sitesSheetId}`
     );
-    const sites = await papaPromise(response.body, options);
-    return sites;
+    return papaPromise(response.body, options);
   };
 
+  const fetchEntitiesSheet = async () => {
+    const options = {
+      ...papaOptions,
+      transformHeader: (header) => camelCase(header.trim()),
+    };
+    const response = await fetch(
+      `${SPREADSHEETS_URL}d/${spreadsheetId}/export?format=csv&gid=${entitiesSheetId}`
+    );
+    return papaPromise(response.body, options);
+  };
+
+  const fetchSite = async () => {
+    const sitesSheets = await fetchSitesSheet();
+    const siteSheet = siteSlug
+      ? sitesSheets.find(
+          (sheet) =>
+            siteSlug.localeCompare(sheet.slug, "en", {
+              sensitivity: "base",
+            }) === 0
+        )
+      : sitesSheets[0];
+    const entitiesSheets = await fetchEntitiesSheet();
+    const entity = entitiesSheets.find(
+      (entitySheet) => entitySheet.name === siteSheet.entity
+    );
+    return { ...siteSheet, entity };
+  };
+
+  const fetchFor = {
+    site: fetchSite,
+  };
+  const gcache = cache({ fetchFor });
   const api = {
-    sites: ({ slug, locale = siteServer.defaultLocale }) => ({
+    sites: () => ({
       get first() {
         return (async () => {
-          return getSites(slug, locale);
+          return gcache.site;
         })();
       },
     }),
