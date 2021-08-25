f28a40c5f18b2d18527f176211a382a5ef05ca5d
_ Kilemensi <clemence@codeforafrica.org>
2021-08-25 15:40:27 +0300

    Initial implementation of caching to publid/data folder

---
+++ src/lib/cache/index.js
@@ -0,0 +1,84 @@
+import fs from "fs";
+import path from "path";
+
+import config from "@/promisetracker/config";
+
+const CACHE_TIMEOUT = process.env.CACHE_TIMEOUT || config.CACHE_TIMEOUT;
+const PROMISES_CACHE_FILENAME = "promises.json";
+const SITES_CACHE_FILENAME = "sites.json";
+
+// async function fetchAllNodes(url, options = { headers }, times = 0) {
+//   const response = await fetch(url, options);
+//   const resjson = await response.json();
+//   const data = resjson.results;
+//   if (resjson.next) {
+//     const nextData = await fetchAllNodes(resjson.next, options, times + 1);
+//     return { ...nextData, results: data.concat(nextData.results) };
+//   }
+
+//   return { ...resjson, results: data };
+// }
+
+// async function fetchNodes(days = 7) {
+//   const toDate = new Date();
+//   const fromDate = toDate.setDate(toDate.getDate() - days);
+//   const lastNotify = new Date(fromDate).toISOString();
+
+//   return fetchAllNodes(
+//     `https://api.sensors.africa/v1/node?last_notify__gte=${lastNotify}`
+//   );
+// }
+
+async function read(filename) {
+  const publicDirectory = path.join(process.cwd(), "public/data");
+  const filePath = path.join(publicDirectory, filename);
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+}
+
+async function write(filename, data) {
+  const publicDirectory = path.join(process.cwd(), "public/data");
+  const filePath = path.join(publicDirectory, filename);
+  fs.writeFileSync(filePath, JSON.stringify(data), "utf8");
+}
+
+async function load(filename, fetchNew) {
+  let cached;
+  try {
+    cached = read(filename);
+  } catch (error) {
+    cached = null;
+  }
+  if (!cached || cached.lastUpdated < Date.now() - CACHE_TIMEOUT * 1000) {
+    cached = await fetchNew();
+    write(PROMISES_CACHE_FILENAME, cached);
+  }
+
+  return cached;
+}
+
+function cache({ fetch }) {
+  const api = {
+    get site() {
+      return (async () => {
+        const fetchNew = async () => {
+          const data = await fetch.site();
+          return { lastUpdated: Date.now(), data };
+        };
+        return load(SITES_CACHE_FILENAME, fetchNew);
+      })();
+    },
+    get promises() {
+      return (async () => {
+        const fetchNew = async () => {
+          const data = await fetch.promises();
+          return { count: data.length, lastUpdated: Date.now(), data };
+        };
+        return load(PROMISES_CACHE_FILENAME, fetchNew);
+      })();
+    },
+  };
+
+  return api;
+}
+
+export default cache;
