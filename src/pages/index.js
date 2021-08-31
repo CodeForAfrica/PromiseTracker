d17ce0c4fa0ea499241011ee95e95a805b429209
_ Kilemensi <clemence@codeforafrica.org>
2021-08-31 08:12:10 +0300

    Include loading of projectMeta for gsheets

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Subscribe from "@/promisetracker/components/Newsletter";
 import Page from "@/promisetracker/components/Page";
 import Partners from "@/promisetracker/components/Partners";
 // import promisesApi from "@/promisetracker/lib/api";
-import gsheetsFn from "@/promisetracker/lib/gsheets";
+import backendFn from "@/promisetracker/lib/backend";
 import i18n from "@/promisetracker/lib/i18n";
 import wp from "@/promisetracker/lib/wp";
 import { groupPromisesByStatus } from "@/promisetracker/utils";
@@ -59,11 +59,11 @@ function Index({
       <Hero
         promisesByStatus={promisesByStatus}
         criteria={criteria}
+        fullName={projectMeta.fullName}
         name={projectMeta.name}
         position={projectMeta.position}
         promiseLabel={projectMeta.promiseLabel}
         updatedAtLabel={projectMeta.updatedAtLabel}
-        trailText={projectMeta.trailText}
         updatedAt={new Date(projectMeta.updatedAt)
           .toDateString({
             dateStyle: "short",
@@ -71,7 +71,9 @@ function Index({
           .split(" ")
           .slice(1)
           .join(" ")}
+        tagline={projectMeta.tagline}
         title={projectMeta.description}
+        trailText={projectMeta.trailText}
         classes={{ section: classes.section }}
       />
       <KeyPromises
@@ -128,13 +130,15 @@ Index.propTypes = {
   navigation: PropTypes.shape({}),
   partners: PropTypes.shape({}),
   projectMeta: PropTypes.shape({
+    description: PropTypes.string,
+    fullName: PropTypes.string,
     name: PropTypes.string,
     position: PropTypes.string,
-    trailText: PropTypes.string,
+    promiseLabel: PropTypes.string,
     updatedAt: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     updatedAtLabel: PropTypes.string,
-    description: PropTypes.string,
-    promiseLabel: PropTypes.string,
+    tagline: PropTypes.string,
+    trailText: PropTypes.string,
   }),
   promiseStatuses: PropTypes.arrayOf(PropTypes.shape({})),
   promises: PropTypes.arrayOf(PropTypes.shape({})),
@@ -169,8 +173,8 @@ export async function getStaticProps({ locale }) {
   const wpApi = wp();
   const page = await wpApi.pages({ slug: "index", locale }).first;
   // const { promiseStatuses } = page;
-  const gsheets = gsheetsFn();
-  const promisesApi = gsheets.promises();
+  const backend = backendFn();
+  const promisesApi = backend.promises();
   // const api = promisesApi({
   //   promiseStatuses,
   //   team: "pesacheck-promise-tracker",
@@ -188,8 +192,9 @@ export async function getStaticProps({ locale }) {
   // });
   const posts = await wpApi.pages({ slug: "analysis-articles", locale }).posts;
   const articles = posts?.slice(0, 4) || null;
-  const projectMeta = {};
   // const projectMeta = await api.projectMeta();
+  const projectApi = backend.project();
+  const projectMeta = await projectApi.meta;
   const languageAlternates = _.languageAlternates();
 
   return {
