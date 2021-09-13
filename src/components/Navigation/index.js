e0552a119a56de541f68ec7d89d40122366385a4
_ Kilemensi <clemence@codeforafrica.org>
2021-09-13 16:26:47 +0300

    handle empty navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,7 @@
 import { AppBar, Hidden, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { isEmpty } from "lodash";
+import PropTypes from "prop-types";
 import React from "react";
 
 import DesktopNavigation from "./DesktopNavigation";
@@ -22,21 +24,38 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
   },
 }));
 
-function Navigation(props) {
+function Navigation({ navigation, ...props }) {
   const classes = useStyles(props);
 
+  if (isEmpty(navigation)) {
+    return null;
+  }
   return (
     <AppBar color="primary" position="sticky" className={classes.root}>
       <Toolbar disableGutters className={classes.toolbar}>
         <Hidden mdDown implementation="css">
-          <DesktopNavigation classes={{ section: classes.section }} />
+          <DesktopNavigation
+            navigation={navigation}
+            classes={{ section: classes.section }}
+          />
         </Hidden>
         <Hidden lgUp implementation="css">
-          <MobileNavigation classes={{ section: classes.section }} />
+          <MobileNavigation
+            navigation={navigation}
+            classes={{ section: classes.section }}
+          />
         </Hidden>
       </Toolbar>
     </AppBar>
   );
 }
+
+Navigation.propTypes = {
+  navigation: PropTypes.shape({}),
+};
+
+Navigation.defaultProps = {
+  navigation: undefined,
+};
 
 export default Navigation;
