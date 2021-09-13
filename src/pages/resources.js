33834df9856e3a9d022e810626640e3a1c23adda
_ Kilemensi <clemence@codeforafrica.org>
2021-09-13 16:30:05 +0300

    Use site navigation

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -5,6 +5,7 @@ import React from "react";
 
 import ActNow from "@/promisetracker/components/ActNow";
 import ContentPage from "@/promisetracker/components/ContentPage";
+import backendFn from "@/promisetracker/lib/backend";
 import i18n from "@/promisetracker/lib/i18n";
 import wp from "@/promisetracker/lib/wp";
 
@@ -82,6 +83,9 @@ export async function getStaticProps({ locale }) {
     };
   }
 
+  const backend = backendFn();
+  const site = await backend.sites().current;
+  const { navigation } = site;
   const page = await wp().pages({ slug: "resources", locale }).first;
   const languageAlternates = _.languageAlternates("/resources");
 
@@ -89,6 +93,7 @@ export async function getStaticProps({ locale }) {
     props: {
       ...page,
       languageAlternates,
+      navigation,
     },
     revalidate: 2 * 60, // seconds
   };
