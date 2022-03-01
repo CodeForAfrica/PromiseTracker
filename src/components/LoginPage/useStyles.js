7286ad64b7430037025fdcaccf472a8ec3dac885
Obed Amoasi <obedamoasi@gmail.com>
2022-03-01 13:34:31 +0000

    use box shadow

--- src/components/LoginPage/useStyles.js
+++ src/components/LoginPage/useStyles.js
@@ -54,7 +54,8 @@ const useStyles = makeStyles(({ palette, typography, breakpoints }) => ({
     alignItems: "center",
   },
   container: {
-    background: "#F7F7F7",
+    boxShadow:
+      "rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px",
     padding: typography.pxToRem(30),
     marginTop: typography.pxToRem(80),
     marginBottom: typography.pxToRem(80),
