06d7e6fbf82029b34240d877e5acba8706b74527
Obed Amoasi <obedamoasi@gmail.com>
2022-03-02 12:47:39 +0000

    [wip] log filepath

--- src/lib/cache/index.js
+++ src/lib/cache/index.js
@@ -16,7 +16,10 @@ function cache(server, fetchFor) {
 
   async function read(filename) {
     const file = getFilePath(filename);
+    console.time(`read ${file}`);
     const data = await readFile(file, { encoding: "utf8" });
+    console.timeEnd(`read ${file}`);
+    console.log(data);
     return JSON.parse(data);
   }
 
