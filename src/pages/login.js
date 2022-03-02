7ad0919f3b4ed946f4fea22e3872df633130d1ae
Obed Amoasi <obedamoasi@gmail.com>
2022-03-02 10:54:25 +0000

    [wip] enable session in login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
-// import { getProviders, getSession } from "next-auth/react";
+import { getProviders, getSession } from "next-auth/react";
 import PropTypes from "prop-types";
 import React from "react";
 
 import LoginPage from "@/promisetracker/components/LoginPage";
 // import Page from "@/promisetracker/components/Page";
 // import backendFn from "@/promisetracker/lib/backend";
-// import i18n from "@/promisetracker/lib/i18n";
-// import wp from "@/promisetracker/lib/wp";
+import i18n from "@/promisetracker/lib/i18n";
+import wp from "@/promisetracker/lib/wp";
 
 const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
   section: {
@@ -50,38 +50,38 @@ Login.defaultProps = {
   providers: undefined,
 };
 
-export async function getServerSideProps() {
-  // const session = await getSession(context);
-  // if (session && session?.user) {
-  //   return {
-  //     redirect: {
-  //       permanent: false,
-  //       destination: "/act-now",
-  //     },
-  //   };
-  // }
-  // const _ = i18n();
-  // // Skip generating pages for unsupported locales
-  // if (!_.locales.includes(locale)) {
-  //   return {
-  //     notFound: true,
-  //   };
-  // }
-  // const wpApi = wp();
+export async function getServerSideProps({ locale, ...context }) {
+  const session = await getSession(context);
+  if (session && session?.user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/act-now",
+      },
+    };
+  }
+  const _ = i18n();
+  // Skip generating pages for unsupported locales
+  if (!_.locales.includes(locale)) {
+    return {
+      notFound: true,
+    };
+  }
+  const wpApi = wp();
   // const backend = backendFn();
-  // const [page, providers, site] = await Promise.all([
-  //   wpApi.pages({ slug: "index", locale }).first,
-  //   getProviders(),
-  //   backend.sites().current,
-  // ]);
+  const [page, providers] = await Promise.all([
+    wpApi.pages({ slug: "index", locale }).first,
+    getProviders(),
+    // backend.sites().current,
+  ]);
 
   // const { navigation } = site;
 
   return {
     props: {
-      // ...page,
+      ...page,
       // navigation,
-      // providers,
+      providers,
       // session,
     },
   };
