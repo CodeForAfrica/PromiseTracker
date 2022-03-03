0a8d9185b9d967d299427313e6cebf668de1570f
Obed Amoasi <obedamoasi@gmail.com>
2022-03-03 06:40:41 +0000

    fix import issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { getProviders, useSession } from "next-auth/react";
-import { Router } from "next/router";
+import Router from "next/router";
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 
