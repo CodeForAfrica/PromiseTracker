f981563175f34899b73135f0344be6d8748110b4
esir <esirkings@gmail.com>
2022-04-01 11:35:45 +0300

    Use afterInteractive strategy

--- src/components/Page/Base.js
+++ src/components/Page/Base.js
@@ -27,15 +27,19 @@ function BasePage({
 
   return (
     <div className={classes.root}>
-      <Script id="google-analytics">
-        {`
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${GTM_ID}');        
-        `}
-      </Script>
+        `,
+        }}
+      />
       <NextSeo title={title} {...props} />
       <Navigation
         navigation={navigation}
