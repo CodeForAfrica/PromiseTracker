a66d99216515c1e2e403f2452bf64e5b6540ad0b
esir <esirkings@gmail.com>
2022-04-01 12:47:51 +0300

    Dynamically add script depending of if GTM is available

--- src/components/Page/Base.js
+++ src/components/Page/Base.js
@@ -12,7 +12,8 @@ import config from "@/promisetracker/config";
 /**
  * Base page that can be used to build all other pages.
  */
-const GTM_ID = process.env.GTM_ID || "GTM-P8QSVKT";
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 function BasePage({
   children,
   footer,
@@ -27,19 +28,23 @@ function BasePage({
 
   return (
     <div className={classes.root}>
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');        
-        `,
-        }}
-      />
+      {GTM_ID ? (
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');        
+          `,
+          }}
+        />
+      ) : (
+        ""
+      )}
       <NextSeo title={title} {...props} />
       <Navigation
         navigation={navigation}
