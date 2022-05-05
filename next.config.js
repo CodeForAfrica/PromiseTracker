e15d8a8e1df9f9eb3f174c8e16156cd3d79380e9
esir <esirkings@gmail.com>
2022-05-05 13:03:36 +0300

    Remove sentry config on non production environment

--- next.config.js
+++ next.config.js
@@ -35,16 +35,21 @@ const moduleExports = {
   },
 };
 
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+const NodeEnv = process.env.ENVIRONMENT || "development";
+if (NodeEnv === "production") {
+  const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+  module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+} else {
+  module.exports = moduleExports;
+}
